9bd38d36d50c17b38c0877c587f06736092a3210
kevinbazira <kvnbzr@gmail.com>
2021-06-01 12:40:28 +0300

    update inference service to work with generic image
    set enwiki-goodfaith inference service config file and model-server to work with a generic kfserving revscoring docker image
    
    Bug: T283526
    Change-Id: I9a4887066561975d150997aca644c5b8e70e6120

--- enwiki-goodfaith/model-server/model.py
+++ enwiki-goodfaith/model-server/model.py
@@ -1,26 +1,27 @@
 import kfserving
-from revscoring import Model
 import mwapi
+import os
+from revscoring import Model
 from revscoring.extractors import api
 from typing import Dict
 
 
-class EnWikiGoodfaithModel(kfserving.KFModel):
+class RevscoringModel(kfserving.KFModel):
     def __init__(self, name: str):
         super().__init__(name)
         self.name = name
         self.ready = False
 
     def load(self):
-        with open("enwiki.goodfaith.gradient_boosting.model") as f:
+        with open("/mnt/models/model.bin") as f:
             self.model = Model.load(f)
+        wiki_url = os.environ.get("WIKI_URL")
         self.extractor = api.Extractor(mwapi.Session(
-            "https://en.wikipedia.org",
+            wiki_url,
             user_agent="KFServing revscoring demo"))
         self.ready = True
 
     def predict(self, request: Dict) -> Dict:
-
         inputs = request["rev_id"]
         feature_values = list(
             self.extractor.extract(inputs, self.model.features))
@@ -29,6 +30,7 @@ class EnWikiGoodfaithModel(kfserving.KFModel):
 
 
 if __name__ == "__main__":
-    model = EnWikiGoodfaithModel("enwiki-goodfaith")
+    inference_name = os.environ.get("INFERENCE_NAME")
+    model = RevscoringModel(inference_name)
     model.load()
     kfserving.KFServer(workers=1).start([model])
