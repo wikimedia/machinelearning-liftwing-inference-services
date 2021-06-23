8a16fca9ea67eca52e388fbeafd0893b75ade00b
kevinbazira <kvnbzr@gmail.com>
2021-06-23 14:39:38 +0300

    add enwiki articlequality inferenceservice
    enwiki articlequality inferenceservice config file using v1beta1 plus a generic kfserving revscoring+articlequality model server
    
    Bug: T284678
    Change-Id: I7c963ddda208de8aa87dc5135711ee7553e79195

---
+++ revscoring/articlequality/model-server/model.py
@@ -0,0 +1,29 @@
+import articlequality
+import kfserving
+import os
+from revscoring import Model
+from typing import Dict
+
+
+class ArticlequalityModel(kfserving.KFModel):
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.name = name
+        self.ready = False
+
+    def load(self):
+        with open("/mnt/models/model.bin") as f:
+            self.model = Model.load(f)
+        self.ready = True
+
+    def predict(self, request: Dict) -> Dict:
+        inputs = request["article_text"]
+        results = articlequality.score(self.model, inputs)
+        return {"predictions": results}
+
+
+if __name__ == "__main__":
+    inference_name = os.environ.get("INFERENCE_NAME")
+    model = ArticlequalityModel(inference_name)
+    model.load()
+    kfserving.KFServer(workers=1).start([model])
