691c29829332a58a3eba4d4d0ec0860112db26ba
kevinbazira <kvnbzr@gmail.com>
2021-08-03 16:51:25 +0300

    add enwiki damaging editquality client
    run a prediction with enwiki damaging editquality inference service
    
    Bug: T287051
    Change-Id: Ifc435bf9306fe2e677eced94b2aae76ac1155711

---
+++ revscoring/editquality/enwiki-damaging/inference-client.sh
@@ -0,0 +1,5 @@
+MODEL_NAME=enwiki-damaging
+INPUT_PATH=@./input.json
+CLIENT_GENERATORS_PATH=../../../client
+SESSION=$(python3 $CLIENT_GENERATORS_PATH/authservice-session-generator.py)
+. $CLIENT_GENERATORS_PATH/inference-generator.sh
