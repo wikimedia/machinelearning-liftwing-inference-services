311f80d1ed8845cdc6caf8d69d03bcc53542ecb6
Andy Craze <acraze@wikimedia.org>
2021-09-24 11:50:32 -0700

    outlink: add blubberfile for model server
    changed the base image to buster due to
    to errors with py3.9 and easy_install on bullseye.
    
    also added test deps for test stage and fixed
    formatting for model.py
    
    Bug: T290929
    Change-Id: I82f6e41bddb817ad00b8a5d15210afd626e4f101

--- outlink-topic-model/model-server/model.py
+++ outlink-topic-model/model-server/model.py
@@ -11,35 +11,39 @@ class OutlinksTopicModel(kfserving.KFModel):
         self.ready = False
 
     def load(self):
-        self.model = fasttext.load_model('/mnt/models/model.bin')
+        self.model = fasttext.load_model("/mnt/models/model.bin")
         self.ready = True
 
     def predict(self, request: Dict) -> Dict:
         features_str = request["features_str"]
         # outlinks = request["outlinks"]
-        threshold = request.get('threshold', 0.5)
-        debug = request.get('debug')
-        lang = request.get('lang')
-        page_title = request.get('page_title')
+        threshold = request.get("threshold", 0.5)
+        debug = request.get("debug")
+        lang = request.get("lang")
+        page_title = request.get("page_title")
         lbls, scores = self.model.predict(features_str, k=-1)
         results = {lb: s for lb, s in zip(lbls, scores)}
-        sorted_res = [(lb.replace("__label__", ""), results[lb])
-                      for lb in sorted(results, key=results.get, reverse=True)]
+        sorted_res = [
+            (lb.replace("__label__", ""), results[lb])
+            for lb in sorted(results, key=results.get, reverse=True)
+        ]
         above_threshold = [r for r in sorted_res if r[1] >= threshold]
         lbls_above_threshold = []
         if above_threshold:
             for res in above_threshold:
                 if debug:
-                    print('{0}: {1:.3f}'.format(*res))
+                    print("{0}: {1:.3f}".format(*res))
                 if res[1] > threshold:
                     lbls_above_threshold.append(res[0])
         elif debug:
             print("No label above {0} threshold.".format(threshold))
-            print("Top result: {0} ({1:.3f}) -- {2}".format(
-                sorted_res[0][0], sorted_res[0][1], sorted_res[0][2]))
+            print(
+                "Top result: {0} ({1:.3f}) -- {2}".format(
+                    sorted_res[0][0], sorted_res[0][1], sorted_res[0][2]
+                )
+            )
 
-        return {"topics": above_threshold, "lang": lang,
-                "page_title": page_title}
+        return {"topics": above_threshold, "lang": lang, "page_title": page_title}
 
 
 if __name__ == "__main__":
