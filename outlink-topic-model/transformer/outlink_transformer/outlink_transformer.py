78514cb52528649ef90108b5aee9ed2079844541
Andy Craze <acraze@wikimedia.org>
2021-11-05 16:43:42 -0700

    outlink: add blubberfile for transformer
    Adding multi-stage builds for outlink topic model
    transformer. Also adding test variant and some
    slight formatting changes using black.
    
    Bug: T290929
    Change-Id: Id98bde37424c441ac0f3d255154a55f4b7a83162

--- outlink-topic-model/transformer/outlink_transformer/outlink_transformer.py
+++ outlink-topic-model/transformer/outlink_transformer/outlink_transformer.py
@@ -1,5 +1,5 @@
 import kfserving
-from typing import Dict
+from typing import Dict, Set
 import logging
 import mwapi
 import os
@@ -7,12 +7,14 @@ import os
 
 logging.basicConfig(level=kfserving.constants.KFSERVING_LOGLEVEL)
 
-def get_outlinks(title, lang, limit=1000, session=None):
+
+def get_outlinks(title: str, lang: str, limit=1000, session=None) -> Set:
     """Gather set of up to `limit` outlinks for an article."""
     if session is None:
         session = mwapi.Session(
-            'https://{0}.wikipedia.org'.format(lang),
-            user_agent=os.environ.get('CUSTOM_UA'))
+            "https://{0}.wikipedia.org".format(lang),
+            user_agent=os.environ.get("CUSTOM_UA"),
+        )
 
     # generate list of all outlinks (to namespace 0) from
     # the article and their associated Wikidata IDs
@@ -20,32 +22,32 @@ def get_outlinks(title, lang, limit=1000, session=None):
         action="query",
         generator="links",
         titles=title,
-        redirects='',
-        prop='pageprops',
-        ppprop='wikibase_item',
+        redirects="",
+        prop="pageprops",
+        ppprop="wikibase_item",
         gplnamespace=0,  # this actually doesn't seem to work :/
         gpllimit=50,
-        format='json',
+        format="json",
         formatversion=2,
-        continuation=True
+        continuation=True,
     )
     logging.info(result)
     try:
         outlink_qids = set()
         for r in result:
-            for outlink in r['query']['pages']:
+            for outlink in r["query"]["pages"]:
                 # namespace 0 and not a red link
-                if outlink['ns'] == 0 and 'missing' not in outlink:
-                    qid = outlink.get('pageprops', {}).get(
-                        'wikibase_item', None)
+                if outlink["ns"] == 0 and "missing" not in outlink:
+                    qid = outlink.get("pageprops", {}).get(
+                        "wikibase_item", None)
                     if qid is not None:
                         outlink_qids.add(qid)
             if len(outlink_qids) > limit:
                 break
         return outlink_qids
     except Exception:
-        logging.error('Could not parse outlinks response')
-        return None
+        logging.error("Could not parse outlinks response")
+        return set()  # return empty set to join on feature_str
 
 
 class OutlinkTransformer(kfserving.KFModel):
@@ -55,20 +57,18 @@ class OutlinkTransformer(kfserving.KFModel):
 
     def preprocess(self, inputs: Dict) -> Dict:
         """Get outlinks and features_str. Returns dict."""
-        lang = inputs.get('lang')
-        page_title = inputs.get('page_title')
+        lang = inputs.get("lang")
+        page_title = inputs.get("page_title")
         outlinks = get_outlinks(page_title, lang)
-        features_str = ' '.join(outlinks)
-        return {'features_str': features_str, 'page_title': page_title,
-                'lang': lang}
+        features_str = " ".join(outlinks)
+        return {"features_str": features_str, "page_title": page_title, "lang": lang}
 
     def postprocess(self, outputs: Dict) -> Dict:
-        topics = outputs['topics']
-        lang = outputs['lang']
-        page_title = outputs['page_title']
-        result = {'article':
-                  'https://{0}.wikipedia.org/wiki/{1}'.format(
-                      lang, page_title),
-                  'results': [{'topic': t[0], 'score': t[1]} for t in topics]
-                  }
-        return {'prediction': result}
+        topics = outputs["topics"]
+        lang = outputs["lang"]
+        page_title = outputs["page_title"]
+        result = {
+            "article": "https://{0}.wikipedia.org/wiki/{1}".format(lang, page_title),
+            "results": [{"topic": t[0], "score": t[1]} for t in topics],
+        }
+        return {"prediction": result}
--- outlink-topic-model/transformer/setup.py
+++ outlink-topic-model/transformer/setup.py
@@ -1,25 +1,12 @@
 from setuptools import setup, find_packages
 
-tests_require = [
-    'pytest',
-    'pytest-tornasync',
-    'mypy'
-]
+tests_require = ["pytest", "pytest-tornasync", "mypy"]
 
 setup(
-    name='outlink-transformer',
-    version='0.1.0',
-    author_email='acraze@wikimedia.org',
-    license='../../COPYING',
-    description='Transformer for Outlink Topic Model',
-    long_description=open('README.md').read(),
-    python_requires='>=3.6',
+    name="outlink-transformer",
+    version="0.1.0",
+    author_email="ml@wikimedia.org",
+    description="Transformer for Outlink Topic Model",
+    python_requires=">=3.6",
     packages=find_packages("outlink-transformer"),
-    install_requires=[
-        "kfserving>=0.2.1",
-        "argparse>=1.4.0",
-        "mwapi"
-    ],
-    tests_require=tests_require,
-    extras_require={'test': tests_require}
 )
