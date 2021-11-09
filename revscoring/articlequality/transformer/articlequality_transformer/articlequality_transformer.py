5a3301a7ac55e5cd45ce05c0f75223c777586c58
Andy Craze <acraze@wikimedia.org>
2021-11-09 15:34:01 -0800

    articlequality: add transformer blubberfile
    adding multi-stage builds for articlequality transformer
    image. Also adding some changes for a test variant and some
    small format changes in the transformer code via black.
    
    Bug: T294419
    Change-Id: Idbbab985675a66500a8ea8c7c1941a088bd8411b

--- revscoring/articlequality/transformer/articlequality_transformer/articlequality_transformer.py
+++ revscoring/articlequality/transformer/articlequality_transformer/articlequality_transformer.py
@@ -16,7 +16,7 @@ class ArticleQualityTransformer(kfserving.KFModel):
 
     def preprocess(self, inputs: Dict) -> Dict:
         """Get outlinks and features_str."""
-        rev_id = inputs.get('rev_id')
+        rev_id = inputs.get("rev_id")
         wiki_url = os.environ.get("WIKI_URL")
         wiki_host = os.environ.get("WIKI_HOST")
         if wiki_host:
@@ -25,27 +25,32 @@ class ArticleQualityTransformer(kfserving.KFModel):
         else:
             s = None
         self.session = mwapi.Session(
-            wiki_url, user_agent="KFServing revscoring demo", session=s)
+            wiki_url, user_agent="KFServing revscoring demo", session=s
+        )
         return self._fetch_articlequality_text(rev_id)
 
     def _fetch_articlequality_text(self, rev_id: int) -> Dict:
         """Retrieve article text features."""
-        doc = self.session.get(action="query", prop="revisions", revids=[rev_id],
-                               rvprop=["ids", "content"], rvslots=["main"],
-                               formatversion=2)
+        doc = self.session.get(
+            action="query",
+            prop="revisions",
+            revids=[rev_id],
+            rvprop=["ids", "content"],
+            rvslots=["main"],
+            formatversion=2,
+        )
 
         try:
-            page_doc = doc['query']['pages'][0]
+            page_doc = doc["query"]["pages"][0]
         except (KeyError, IndexError) as e:
-            logging.error('No Pages found', str(e))
+            logging.error("No Pages found", str(e))
             raise  # propagate exception again
         try:
-            rev_doc = page_doc['revisions'][0]
-            rev_doc['page'] = {k: v for k, v in page_doc.items()
-                               if k != "revisions"}
+            rev_doc = page_doc["revisions"][0]
+            rev_doc["page"] = {k: v for k, v in page_doc.items() if k != "revisions"}
         except (KeyError, IndexError) as e:
-            logging.error('No revisions matched', str(e))
+            logging.error("No revisions matched", str(e))
             raise  # propagate exception again
 
-        content = rev_doc['slots']["main"].get("content")
-        return {'article_text': content}
+        content = rev_doc["slots"]["main"].get("content")
+        return {"article_text": content}
--- revscoring/articlequality/transformer/setup.py
+++ revscoring/articlequality/transformer/setup.py
@@ -2,15 +2,10 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='articlequality-transformer',
-    version='0.1.0',
-    author_email='ml@wikimedia.org',
-    description='Transformer for articlequality models',
-    python_requires='>=3.6',
-    packages=find_packages("articlequality-transformer"),
-    install_requires=[
-        "kfserving>=0.3.0",
-        "argparse>=1.4.0",
-        "mwapi"
-    ]
+    name="articlequality-transformer",
+    version="0.1.0",
+    author_email="ml@wikimedia.org",
+    description="Transformer for articlequality models",
+    python_requires=">=3.6",
+    packages=find_packages("articlequality-transformer")
 )
