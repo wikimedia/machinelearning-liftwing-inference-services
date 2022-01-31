2c16c1546e8158f657a0096efe41188d5635e82c
kevinbazira <kvnbzr@gmail.com>
2022-01-31 11:34:35 +0300

    editquality: adding model to transformer
    load model in transformer. it is needed
    for feature extraction.
    
    Bug: T298943
    Change-Id: Ia3e9a2131ef6abb9c4d1e56bb65b7cac8cb94efd

--- revscoring/editquality/transformer/editquality_transformer/editquality_transformer.py
+++ revscoring/editquality/transformer/editquality_transformer/editquality_transformer.py
@@ -1,10 +1,11 @@
 import kserve
-from typing import Dict
 import logging
 import mwapi
 import os
 import requests
+from revscoring import Model
 from revscoring.extractors import api
+from typing import Dict
 
 
 logging.basicConfig(level=kserve.constants.KSERVE_LOGLEVEL)
@@ -14,6 +15,8 @@ class EditQualityTransformer(kserve.KFModel):
     def __init__(self, name: str, predictor_host: str):
         super().__init__(name)
         self.predictor_host = predictor_host
+        self.ready = False  # ensure we can load model
+        self._load()
 
     def preprocess(self, inputs: Dict) -> Dict:
         """Use MW API session and Revscoring API to extract feature values
@@ -26,10 +29,20 @@ class EditQualityTransformer(kserve.KFModel):
             s.headers.update({"Host": wiki_host})
         else:
             s = None
-        extractor = api.Extractor(
+        self.extractor = api.Extractor(
             mwapi.Session(
                 wiki_url, user_agent="WMF ML Team editquality model", session=s
             )
         )
-        feature_values = list(extractor.extract(rev_id, self.model.features))
+        return self._fetch_editquality_features(rev_id)
+
+    def _load(self):
+        """Load model so we can access features."""
+        with open("/mnt/models/model.bin") as f:
+            self.model = Model.load(f)
+        self.ready = True
+
+    def _fetch_editquality_features(self, rev_id: int) -> Dict:
+        """Retrieve editquality features."""
+        feature_values = list(self.extractor.extract(rev_id, self.model.features))
         return {"feature_values": feature_values}
