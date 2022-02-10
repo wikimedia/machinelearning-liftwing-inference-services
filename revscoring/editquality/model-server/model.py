33fabfb0eafb5760c46e36649c5c57d1e9c6f263
kevinbazira <kvnbzr@gmail.com>
2022-02-10 13:33:19 +0300

    editquality: remove transformer code
    reverting back to the predictor-only design
    as the transformer was also loading
    models + revscoring and all its dependenies.
    the approach in this patch keeps things DRY
    and saves compute resources.
    
    Bug: T301412
    Change-Id: I33e55291d082d966525d6c30fb55d180e07a73af

--- revscoring/editquality/model-server/model.py
+++ revscoring/editquality/model-server/model.py
@@ -1,6 +1,9 @@
 import kserve
+import mwapi
 import os
+import requests
 from revscoring import Model
+from revscoring.extractors import api
 from typing import Dict
 
 
@@ -15,8 +18,34 @@ class EditQualityModel(kserve.KFModel):
             self.model = Model.load(f)
         self.ready = True
 
+    def preprocess(self, inputs: Dict) -> Dict:
+        """Use MW API session and Revscoring API to extract feature values
+        of edit text based on its revision id"""
+        rev_id = inputs.get("rev_id")
+        wiki_url = os.environ.get("WIKI_URL")
+        wiki_host = os.environ.get("WIKI_HOST")
+
+        if wiki_host:
+            s = requests.Session()
+            s.headers.update({"Host": wiki_host})
+        else:
+            s = None
+
+        self.extractor = api.Extractor(
+            mwapi.Session(
+                wiki_url, user_agent="WMF ML Team editquality model", session=s
+            )
+        )
+
+        return self.fetch_editquality_features(rev_id)
+
+    def fetch_editquality_features(self, rev_id: int) -> Dict:
+        """Retrieve editquality features."""
+        feature_values = list(self.extractor.extract(rev_id, self.model.features))
+        return feature_values
+
     def predict(self, request: Dict) -> Dict:
-        feature_values = request["feature_values"]
+        feature_values = self.preprocess(request)
         results = self.model.score(feature_values)
         return {"predictions": results}
 
