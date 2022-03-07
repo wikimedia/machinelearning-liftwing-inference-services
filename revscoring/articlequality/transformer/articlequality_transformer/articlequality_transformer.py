97fcf861106c7c57664f06020110a18024ec9d8a
Andy Craze <acraze@wikimedia.org>
2022-03-07 13:22:52 -0800

    articlequality: add http error handling
    adding meaningful http error responses
    to the transformer service.
    
    Bug: T300270
    Change-Id: I80ca0eb70e4a6e0cde9cd3bcee6394870b002ae8

--- revscoring/articlequality/transformer/articlequality_transformer/articlequality_transformer.py
+++ revscoring/articlequality/transformer/articlequality_transformer/articlequality_transformer.py
@@ -1,10 +1,12 @@
-import kserve
-from typing import Dict
 import logging
-import mwapi
 import os
-import requests
+from http import HTTPStatus
+from typing import Dict
 
+import kserve
+import mwapi
+import requests
+import tornado.web
 
 logging.basicConfig(level=kserve.constants.KSERVE_LOGLEVEL)
 
@@ -15,8 +17,8 @@ class ArticleQualityTransformer(kserve.KFModel):
         self.predictor_host = predictor_host
 
     def preprocess(self, inputs: Dict) -> Dict:
-        """Get outlinks and features_str."""
-        rev_id = inputs.get("rev_id")
+        """Fetch article text"""
+        rev_id = self._get_rev_id(inputs)
         wiki_url = os.environ.get("WIKI_URL")
         wiki_host = os.environ.get("WIKI_HOST")
         if wiki_host:
@@ -28,6 +30,21 @@ class ArticleQualityTransformer(kserve.KFModel):
             wiki_url, user_agent="WMF ML team articlequality transformer", session=s
         )
         return self._fetch_articlequality_text(rev_id)
+
+    def _get_rev_id(self, inputs: Dict) -> Dict:
+        try:
+            rev_id = inputs["rev_id"]
+        except KeyError:
+            raise tornado.web.HTTPError(
+                status_code=HTTPStatus.BAD_REQUEST,
+                reason='Expected "rev_id" in input data.',
+            )
+        if not isinstance(rev_id, int):
+            raise tornado.web.HTTPError(
+                status_code=HTTPStatus.BAD_REQUEST,
+                reason='Expected "rev_id" to be an integer.',
+            )
+        return rev_id
 
     def _fetch_articlequality_text(self, rev_id: int) -> Dict:
         """Retrieve article text features."""
