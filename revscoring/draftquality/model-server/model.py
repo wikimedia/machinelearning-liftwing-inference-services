1ff17a01a2e5d2882a728cad1da8554f6da52090
Andy Craze <acraze@wikimedia.org>
2022-03-07 12:44:47 -0800

    draftquality: remove transformer code
    reverting back to non-transformer architecture
    
    Bug: T294419
    Change-Id: I52402e1e5737a85784f3d7d532dd84fb4537e05c

--- revscoring/draftquality/model-server/model.py
+++ revscoring/draftquality/model-server/model.py
@@ -1,8 +1,12 @@
 import bz2
-import kserve
 import os
-from revscoring import Model
 from typing import Dict
+
+import kserve
+import mwapi
+import requests
+from revscoring import Model
+from revscoring.extractors import api
 
 
 class DraftqualityModel(kserve.KFModel):
@@ -10,16 +14,39 @@ class DraftqualityModel(kserve.KFModel):
         super().__init__(name)
         self.name = name
         self.ready = False
+        self.FEATURE_VAL_KEY = "feature_values"
 
     def load(self):
         with bz2.open("/mnt/models/model.bz2") as f:
             self.model = Model.load(f)
         self.ready = True
 
+    def preprocess(self, inputs: Dict) -> Dict:
+        """Retrieve features from mediawiki."""
+        rev_id = inputs.get("rev_id")
+        wiki_url = os.environ.get("WIKI_URL")
+        wiki_host = os.environ.get("WIKI_HOST")
+        if wiki_host:
+            s = requests.Session()
+            s.headers.update({"Host": wiki_host})
+        else:
+            s = None
+        ua = "WMF ML team draftquality model"
+        self.extractor = api.Extractor(
+            mwapi.Session(wiki_url, user_agent=ua, session=s)
+        )
+        inputs[self.FEATURE_VAL_KEY] = self._fetch_draftquality_features(rev_id)
+        return inputs
+
     def predict(self, request: Dict) -> Dict:
-        feature_values = request["feature_values"]
+        feature_values = request[self.FEATURE_VAL_KEY]
         results = self.model.score(feature_values)
         return {"predictions": results}
+
+    def _fetch_draftquality_features(self, rev_id: int) -> Dict:
+        """Retrieve draftquality features."""
+        feature_values = list(self.extractor.extract(rev_id, self.model.features))
+        return feature_values
 
 
 if __name__ == "__main__":
