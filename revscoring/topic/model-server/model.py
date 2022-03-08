2d7b220ca9ca9b5523615fd16cd554946e4f6c82
Andy Craze <acraze@wikimedia.org>
2022-03-08 13:04:02 -0800

    topic: revert to non-transformer architecture
    reverting back to predictor-only isvc
    
    Bug: T294419
    Change-Id: Iff384a274eb85b5502e12180753b415994225440

--- revscoring/topic/model-server/model.py
+++ revscoring/topic/model-server/model.py
@@ -1,7 +1,11 @@
-import kserve
 import os
-from revscoring import Model
 from typing import Dict
+
+import kserve
+import mwapi
+import requests
+from revscoring import Model
+from revscoring.extractors import api
 
 
 class DrafttopicModel(kserve.KFModel):
@@ -9,16 +13,39 @@ class DrafttopicModel(kserve.KFModel):
         super().__init__(name)
         self.name = name
         self.ready = False
+        self.FEATURE_VAL_KEY = "feature_values"
 
     def load(self):
         with open("/mnt/models/model.bin") as f:
             self.model = Model.load(f)
         self.ready = True
 
+    def preprocess(self, inputs: Dict) -> Dict:
+        """Use MW API session and Revscoring API to extract feature values
+        of edit text based on its revision id"""
+        rev_id = inputs.get("rev_id")
+        wiki_url = os.environ.get("WIKI_URL")
+        wiki_host = os.environ.get("WIKI_HOST")
+        if wiki_host:
+            s = requests.Session()
+            s.headers.update({"Host": wiki_host})
+        else:
+            s = None
+        self.extractor = api.Extractor(
+            mwapi.Session(wiki_url, user_agent="WMF ML Team topic model", session=s)
+        )
+        inputs[self.FEATURE_VAL_KEY] = self._fetch_topic_features(rev_id)
+        return inputs
+
     def predict(self, request: Dict) -> Dict:
-        feature_values = request["feature_values"]
+        feature_values = request[self.FEATURE_VAL_KEY]
         results = self.model.score(feature_values)
         return {"predictions": results}
+
+    def _fetch_topic_features(self, rev_id: int) -> Dict:
+        """Retrieve topic features."""
+        feature_values = list(self.extractor.extract(rev_id, self.model.features))
+        return feature_values
 
 
 if __name__ == "__main__":
