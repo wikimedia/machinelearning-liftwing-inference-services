f6956e85aa3b3c04ed96e6dd17eb5431a36e8b0a
Aiko Chou <achou@wikimedia.org>
2022-04-07 11:50:04 +0200

    topic: add the ORES augmented feature output
    Supports the ORES augmented feature output in liftwing.
    
    Bug: T301766
    Change-Id: Ic935c6d6dd68d93edd78c777d49043d542c949c5

--- revscoring/topic/model-server/model.py
+++ revscoring/topic/model-server/model.py
@@ -9,6 +9,7 @@ import tornado.web
 from revscoring import Model
 from revscoring.errors import RevisionNotFound
 from revscoring.extractors import api
+from revscoring.features import trim
 
 
 class DrafttopicModel(kserve.KFModel):
@@ -17,6 +18,7 @@ class DrafttopicModel(kserve.KFModel):
         self.name = name
         self.ready = False
         self.FEATURE_VAL_KEY = "feature_values"
+        self.EXTENDED_OUTPUT_KEY = "extended_output"
 
     def load(self):
         with open("/mnt/models/model.bin") as f:
@@ -27,6 +29,7 @@ class DrafttopicModel(kserve.KFModel):
         """Use MW API session and Revscoring API to extract feature values
         of edit text based on its revision id"""
         rev_id = self._get_rev_id(inputs)
+        extended_output = inputs.get("extended_output", False)
         wiki_url = os.environ.get("WIKI_URL")
         wiki_host = os.environ.get("WIKI_HOST")
         if wiki_host:
@@ -38,11 +41,27 @@ class DrafttopicModel(kserve.KFModel):
             mwapi.Session(wiki_url, user_agent="WMF ML Team topic model", session=s)
         )
         inputs[self.FEATURE_VAL_KEY] = self._fetch_topic_features(rev_id)
+        if extended_output:
+            base_feature_values = self.extractor.extract(
+                rev_id, list(trim(self.model.features))
+            )
+            inputs[self.EXTENDED_OUTPUT_KEY] = {
+                str(f): v
+                for f, v in zip(list(trim(self.model.features)), base_feature_values)
+            }
         return inputs
 
     def predict(self, request: Dict) -> Dict:
-        feature_values = request[self.FEATURE_VAL_KEY]
+        feature_values = request.get(self.FEATURE_VAL_KEY)
+        extended_output = request.get(self.EXTENDED_OUTPUT_KEY)
         results = self.model.score(feature_values)
+        if extended_output:
+            # add extended output to reach feature parity with ORES, like:
+            # https://ores.wikimedia.org/v3/scores/enwiki/1083325118/articletopic?features
+            # If only rev_id is given in input.json, only the prediction results
+            # will be present in the response. If the extended_output flag is true,
+            # features output will be included in the response.
+            return {"predictions": results, "features": extended_output}
         return {"predictions": results}
 
     def _get_rev_id(self, inputs: Dict) -> Dict:
