eefec788920c095821fc66330ab807c389450b03
Aiko Chou <achou@wikimedia.org>
2022-04-07 10:59:14 +0200

    draftquality: add the ORES augmented feature output
    Supports the ORES augmented feature output in liftwing.
    
    Bug: T301766
    Change-Id: Ic95afd9236c1d51f3b4338f1b50e7747c33b0d28

--- revscoring/draftquality/model-server/model.py
+++ revscoring/draftquality/model-server/model.py
@@ -10,6 +10,7 @@ import tornado.web
 from revscoring import Model
 from revscoring.errors import RevisionNotFound
 from revscoring.extractors import api
+from revscoring.features import trim
 
 
 class DraftqualityModel(kserve.KFModel):
@@ -18,6 +19,7 @@ class DraftqualityModel(kserve.KFModel):
         self.name = name
         self.ready = False
         self.FEATURE_VAL_KEY = "feature_values"
+        self.EXTENDED_OUTPUT_KEY = "extended_output"
 
     def load(self):
         with bz2.open("/mnt/models/model.bz2") as f:
@@ -27,6 +29,7 @@ class DraftqualityModel(kserve.KFModel):
     def preprocess(self, inputs: Dict) -> Dict:
         """Retrieve features from mediawiki."""
         rev_id = self._get_rev_id(inputs)
+        extended_output = inputs.get("extended_output", False)
         wiki_url = os.environ.get("WIKI_URL")
         wiki_host = os.environ.get("WIKI_HOST")
         if wiki_host:
@@ -39,11 +42,27 @@ class DraftqualityModel(kserve.KFModel):
             mwapi.Session(wiki_url, user_agent=ua, session=s)
         )
         inputs[self.FEATURE_VAL_KEY] = self._fetch_draftquality_features(rev_id)
+        if extended_output:
+            base_feature_values = self.extractor.extract(
+                rev_id, list(trim(self.model.features))
+            )
+            inputs[self.EXTENDED_OUTPUT_KEY] = {
+                str(f): v
+                for f, v in zip(list(trim(self.model.features)), base_feature_values)
+            }
         return inputs
 
     def predict(self, request: Dict) -> Dict:
-        feature_values = request[self.FEATURE_VAL_KEY]
+        feature_values = request.get(self.FEATURE_VAL_KEY)
+        extended_output = request.get(self.EXTENDED_OUTPUT_KEY)
         results = self.model.score(feature_values)
+        if extended_output:
+            # add extended output to reach feature parity with ORES, like:
+            # https://ores.wikimedia.org/v3/scores/enwiki/1083325118/draftquality?features
+            # If only rev_id is given in input.json, only the prediction results
+            # will be present in the response. If the extended_output flag is true,
+            # features output will be included in the response.
+            return {"predictions": results, "features": extended_output}
         return {"predictions": results}
 
     def _fetch_draftquality_features(self, rev_id: int) -> Dict:
