613e7618648557ea3d782d8d12a18b1e038134d6
Aiko Chou <achou@wikimedia.org>
2022-04-21 09:43:10 +0200

    outlink: handle http bad request
    Throw a 400 if the client sends incorrect input data.
    Possible error cases are:
    * no matching article for the given title
    * missing a lang or a title
    * incorrect argument name e.g. lang vs language
    * threshold value provided not a float
    Note: added threshold and debug parameters that were missing here.
    
    Bug: T306029
    Change-Id: Ie8b442554d3890d46a5be6c5628825a203766380

--- outlink-topic-model/transformer/outlink_transformer/outlink_transformer.py
+++ outlink-topic-model/transformer/outlink_transformer/outlink_transformer.py
@@ -3,7 +3,8 @@ from typing import Dict, Set
 import logging
 import mwapi
 import os
-
+import tornado.web
+from http import HTTPStatus
 
 logging.basicConfig(level=kserve.constants.KSERVE_LOGLEVEL)
 
@@ -31,7 +32,6 @@ def get_outlinks(title: str, lang: str, limit=1000, session=None) -> Set:
         formatversion=2,
         continuation=True,
     )
-    logging.info(result)
     try:
         outlink_qids = set()
         for r in result:
@@ -44,8 +44,19 @@ def get_outlinks(title: str, lang: str, limit=1000, session=None) -> Set:
             if len(outlink_qids) > limit:
                 break
         return outlink_qids
+    except KeyError:
+        # either the page doesn't have any outlinks or the page
+        # doesn’t exist. The former case is rare according to
+        # https://en.wikipedia.org/wiki/Category:All_dead-end_pages
+        logging.error("Outlinks response: {}".format(r), exc_info=True)
+        raise tornado.web.HTTPError(
+            status_code=HTTPStatus.BAD_REQUEST,
+            reason="no matching article for https://{0}.wikipedia.org/wiki/{1}".format(
+                lang, title
+            ),
+        )
     except Exception:
-        logging.error("Could not parse outlinks response")
+        logging.error("Outlinks response: {}".format(r), exc_info=True)
         return set()  # return empty set to join on feature_str
 
 
@@ -56,11 +67,41 @@ class OutlinkTransformer(kserve.KFModel):
 
     def preprocess(self, inputs: Dict) -> Dict:
         """Get outlinks and features_str. Returns dict."""
-        lang = inputs.get("lang")
-        page_title = inputs.get("page_title")
+        try:
+            lang = inputs["lang"]
+        except KeyError:
+            raise tornado.web.HTTPError(
+                status_code=HTTPStatus.BAD_REQUEST,
+                reason='missing "lang" in input data.',
+            )
+        try:
+            page_title = inputs["page_title"]
+        except KeyError:
+            raise tornado.web.HTTPError(
+                status_code=HTTPStatus.BAD_REQUEST,
+                reason='missing "page_title" in input data.',
+            )
+        threshold = inputs.get("threshold", 0.5)
+        if not isinstance(threshold, float):
+            raise tornado.web.HTTPError(
+                status_code=HTTPStatus.BAD_REQUEST,
+                reason="threshold value provided not a float",
+            )
+        debug = inputs.get("debug", False)
+        if debug:
+            # when debug is enabled, we want to return all the
+            # predicted topics, so it sets the threshold to 0
+            debug = True
+            threshold = 0.0
         outlinks = get_outlinks(page_title, lang)
         features_str = " ".join(outlinks)
-        return {"features_str": features_str, "page_title": page_title, "lang": lang}
+        return {
+            "features_str": features_str,
+            "page_title": page_title,
+            "lang": lang,
+            "threshold": threshold,
+            "debug": debug,
+        }
 
     def postprocess(self, outputs: Dict) -> Dict:
         topics = outputs["topics"]
