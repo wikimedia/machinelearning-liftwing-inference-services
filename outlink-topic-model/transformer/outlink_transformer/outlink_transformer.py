367c8b386e6ac71c7d6197ac673d1d6f36593bbb
Aiko Chou <achou@wikimedia.org>
2022-06-21 15:52:51 +0200

    outlink: use async HTTP calls to fetch data
    Change to use async session to fetch data. Also add
    a pytest script to test get_outlinks function.
    
    Bug: T311043
    Change-Id: I792eb740b02b8629b81894587d4361b37e37231e

--- outlink-topic-model/transformer/outlink_transformer/outlink_transformer.py
+++ outlink-topic-model/transformer/outlink_transformer/outlink_transformer.py
@@ -5,36 +5,39 @@ import mwapi
 import os
 import tornado.web
 from http import HTTPStatus
+import asyncio
+import aiohttp
 
 logging.basicConfig(level=kserve.constants.KSERVE_LOGLEVEL)
 
 
-def get_outlinks(title: str, lang: str, limit=1000, session=None) -> Set:
+async def get_outlinks(title: str, lang: str, limit=1000) -> Set:
     """Gather set of up to `limit` outlinks for an article."""
-    if session is None:
-        session = mwapi.Session(
+    async with aiohttp.ClientSession() as s:
+        session = mwapi.AsyncSession(
             "https://{0}.wikipedia.org".format(lang),
             user_agent=os.environ.get("CUSTOM_UA"),
+            session=s,
         )
-
-    # generate list of all outlinks (to namespace 0) from
-    # the article and their associated Wikidata IDs
-    result = session.get(
-        action="query",
-        generator="links",
-        titles=title,
-        redirects="",
-        prop="pageprops",
-        ppprop="wikibase_item",
-        gplnamespace=0,  # this actually doesn't seem to work :/
-        gpllimit=50,
-        format="json",
-        formatversion=2,
-        continuation=True,
-    )
-    try:
+        # generate list of all outlinks (to namespace 0) from
+        # the article and their associated Wikidata IDs
+        result = await asyncio.create_task(
+            session.get(
+                action="query",
+                generator="links",
+                titles=title,
+                redirects="",
+                prop="pageprops",
+                ppprop="wikibase_item",
+                gplnamespace=0,
+                gpllimit=50,
+                format="json",
+                formatversion=2,
+                continuation=True,
+            )
+        )
         outlink_qids = set()
-        for r in result:
+        async for r in result:
             for outlink in r["query"]["pages"]:
                 # namespace 0 and not a red link
                 if outlink["ns"] == 0 and "missing" not in outlink:
@@ -44,20 +47,6 @@ def get_outlinks(title: str, lang: str, limit=1000, session=None) -> Set:
             if len(outlink_qids) > limit:
                 break
         return outlink_qids
-    except KeyError:
-        # either the page doesn't have any outlinks or the page
-        # doesn’t exist. The former case is rare according to
-        # https://en.wikipedia.org/wiki/Category:All_dead-end_pages
-        logging.error("Outlinks response: {}".format(r), exc_info=True)
-        raise tornado.web.HTTPError(
-            status_code=HTTPStatus.BAD_REQUEST,
-            reason="no matching article for https://{0}.wikipedia.org/wiki/{1}".format(
-                lang, title
-            ),
-        )
-    except Exception:
-        logging.error("Outlinks response: {}".format(r), exc_info=True)
-        return set()  # return empty set to join on feature_str
 
 
 class OutlinkTransformer(kserve.Model):
@@ -65,27 +54,27 @@ class OutlinkTransformer(kserve.Model):
         super().__init__(name)
         self.predictor_host = predictor_host
 
-    def preprocess(self, inputs: Dict) -> Dict:
+    async def preprocess(self, inputs: Dict) -> Dict:
         """Get outlinks and features_str. Returns dict."""
         try:
             lang = inputs["lang"]
         except KeyError:
             raise tornado.web.HTTPError(
                 status_code=HTTPStatus.BAD_REQUEST,
-                reason='missing "lang" in input data.',
+                reason='Missing "lang" in input data.',
             )
         try:
             page_title = inputs["page_title"]
         except KeyError:
             raise tornado.web.HTTPError(
                 status_code=HTTPStatus.BAD_REQUEST,
-                reason='missing "page_title" in input data.',
+                reason='Missing "page_title" in input data.',
             )
         threshold = inputs.get("threshold", 0.5)
         if not isinstance(threshold, float):
             raise tornado.web.HTTPError(
                 status_code=HTTPStatus.BAD_REQUEST,
-                reason="threshold value provided not a float",
+                reason="Threshold value provided not a float",
             )
         debug = inputs.get("debug", False)
         if debug:
@@ -93,7 +82,24 @@ class OutlinkTransformer(kserve.Model):
             # predicted topics, so it sets the threshold to 0
             debug = True
             threshold = 0.0
-        outlinks = get_outlinks(page_title, lang)
+        try:
+            outlinks = await get_outlinks(page_title, lang)
+        except KeyError:
+            # No matching article or the page has no outlinks
+            raise tornado.web.HTTPError(
+                status_code=HTTPStatus.BAD_REQUEST,
+                reason="No matching article or the page has no outlinks",
+            )
+        except RuntimeError:
+            logging.error(
+                "MediaWiki returned an error."
+                " lang - {}, title - {}".format(lang, page_title),
+                exc_info=True,
+            )
+            raise tornado.web.HTTPError(
+                status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+                reason="An internal error encountered. Please contact the WMF ML team.",
+            )
         features_str = " ".join(outlinks)
         return {
             "features_str": features_str,
--- outlink-topic-model/transformer/setup.py
+++ outlink-topic-model/transformer/setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 tests_require = ["pytest", "pytest-tornasync", "mypy"]
 
 setup(
-    name="outlink-transformer",
+    name="outlink_transformer",
     version="0.1.0",
     author_email="ml@wikimedia.org",
     description="Transformer for Outlink Topic Model",
     python_requires=">=3.6",
-    packages=find_packages("outlink-transformer"),
+    packages=find_packages("outlink_transformer"),
 )
