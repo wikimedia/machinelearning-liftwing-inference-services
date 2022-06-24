d1a48e55be91c0f22c3ad08ebf71fc71ee065e00
Luca Toscano <ltoscano@wikimedia.org>
2022-06-24 16:13:52 +0200

    editquality: add support for revision-score events
    This change adds the first basic support for the generation
    of revision-score events. The functionality is already present
    in ORES, where the /precache URI expects a revision-create
    event JSON and it returns a revision-score event.
    
    The idea is to have Lift Wing to emit events directly
    to EventGate, without the need of ChangeProp in the middle
    (or other tools like it).
    
    The idea is to do the following:
    - Create a new EventGate Stream called mediawiki.revision-score-test.
    - Use Airflow or something similar to listen to revision-create
      events, and contact the Lift Wing API with a special parameter.
    - Check the kafka topic mediawiki.revision-score-test to see if
      events are correctly validated/generated etc..
    
    Bug: T301878
    Change-Id: Ia7d83b3b9a379079d17d93f2ede385a301b35c01

--- revscoring/editquality/model-server/model.py
+++ revscoring/editquality/model-server/model.py
@@ -1,15 +1,20 @@
+import logging
 import os
+import urllib
 from http import HTTPStatus
-from typing import Dict
+from typing import Dict, Optional
 
 import kserve
 import mwapi
 import requests
 import tornado.web
+import tornado.httpclient
 from revscoring import Model
 from revscoring.errors import RevisionNotFound
 from revscoring.extractors import api
 from revscoring.features import trim
+
+logging.basicConfig(level=kserve.constants.KSERVE_LOGLEVEL)
 
 
 class EditQualityModel(kserve.KFModel):
@@ -19,6 +24,12 @@ class EditQualityModel(kserve.KFModel):
         self.ready = False
         self.FEATURE_VAL_KEY = "feature_values"
         self.EXTENDED_OUTPUT_KEY = "extended_output"
+        self.REVISION_CREATE_EVENT_KEY = "revision_create_event"
+        self.EVENTGATE_URL = os.environ.get("EVENTGATE_URL")
+        self.EVENTGATE_STREAM = os.environ.get("EVENTGATE_STREAM")
+        self.CUSTOM_UA = "WMF ML Team editquality model svc"
+        # Deployed via the wmf-certificates package
+        self.TLS_CERT_BUNDLE_PATH = "/etc/ssl/certs/wmf-ca-certificates.crt"
 
     def load(self):
         with open("/mnt/models/model.bin") as f:
@@ -29,6 +40,9 @@ class EditQualityModel(kserve.KFModel):
         """Use MW API session and Revscoring API to extract feature values
         of edit text based on its revision id"""
         rev_id = self._get_rev_id(inputs)
+        # The postprocess() function needs to parse the revision_create_event
+        # given as input (if any).
+        self.revision_create_event = self._get_revision_event(inputs)
         extended_output = inputs.get("extended_output", False)
         wiki_url = os.environ.get("WIKI_URL")
         wiki_host = os.environ.get("WIKI_HOST")
@@ -40,9 +54,7 @@ class EditQualityModel(kserve.KFModel):
             s = None
 
         self.extractor = api.Extractor(
-            mwapi.Session(
-                wiki_url, user_agent="WMF ML Team editquality model", session=s
-            )
+            mwapi.Session(wiki_url, user_agent=self.CUSTOM_UA, session=s)
         )
         inputs[self.FEATURE_VAL_KEY] = self.fetch_editquality_features(rev_id)
         if extended_output:
@@ -55,9 +67,21 @@ class EditQualityModel(kserve.KFModel):
             }
         return inputs
 
+    def _get_revision_event(self, inputs: Dict) -> Optional[str]:
+        try:
+            return inputs[self.REVISION_CREATE_EVENT_KEY]
+        except KeyError:
+            return None
+
     def _get_rev_id(self, inputs: Dict) -> Dict:
         try:
-            rev_id = inputs["rev_id"]
+            # If a revision create event is passed as input,
+            # its rev-id is considerate the one to score.
+            # Otherwise, we look for a specific "rev_id" input.
+            if self.REVISION_CREATE_EVENT_KEY in inputs.keys():
+                rev_id = inputs[self.REVISION_CREATE_EVENT_KEY]["rev_id"]
+            else:
+                rev_id = inputs["rev_id"]
         except KeyError:
             raise tornado.web.HTTPError(
                 status_code=HTTPStatus.BAD_REQUEST,
@@ -82,18 +106,99 @@ class EditQualityModel(kserve.KFModel):
 
         return feature_values
 
-    def predict(self, request: Dict) -> Dict:
+    def get_revision_score_event(self, rev_create_event) -> Dict:
+        if "goodfaith" in self.name:
+            model_name = "goodfaith"
+        elif "damaging" in self.name:
+            model_name = "damaging"
+        else:
+            model_name = "reverted"
+        return {
+            "$schema": "/mediawiki/revision/score/2.0.0",
+            "meta": {
+                "stream": self.EVENTGATE_STREAM,
+            },
+            "database": rev_create_event["database"],
+            "page_id": rev_create_event["page_id"],
+            "page_title": rev_create_event["page_title"],
+            "page_namespace": rev_create_event["page_namespace"],
+            "page_is_redirect": rev_create_event["page_is_redirect"],
+            "performer": rev_create_event["performer"],
+            "rev_id": rev_create_event["rev_id"],
+            "rev_parent_id": rev_create_event["rev_parent_id"],
+            "rev_timestamp": rev_create_event["rev_timestamp"],
+            model_name: {
+                "model_name": model_name,
+                "model_version": self.model.version,
+                "predictions": self.prediction_results,
+            },
+        }
+
+    async def send_event(self, revision_create_event):
+        # TODO: check if the revision_create_event is well formed,
+        # maybe checking the schema and report an error if not revision/create.
+        revision_score_event = self.get_revision_score_event(revision_create_event)
+        try:
+            response = await self._http_client.fetch(
+                self.EVENTGATE_URL,
+                method="POST",
+                ca_certs=self.TLS_CERT_BUNDLE_PATH,
+                body=urllib.parse.urlencode(revision_score_event),
+                user_agent=os.environ.get("CUSTOM_UA"),
+            )
+            logging.debug(
+                "Successfully sent the following event to "
+                "EventGate: {}".format(revision_score_event)
+            )
+        except tornado.httpclient.HTTPError as e:
+            logging.error(
+                "The revision score event has been rejected by EventGate, "
+                "that returned a non-200 HTTP return code "
+                "with the following error: {}".format(e)
+            )
+            raise tornado.web.HTTPError(
+                status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+                reason=(
+                    "Eventgate rejected the revision-score event "
+                    "(a non-HTTP-200 response was returned). "
+                    "Please contact the ML team for more info."
+                ),
+            )
+        except Exception as e:
+            logging.error(
+                "Unexpected error while trying to send a revision score "
+                "event to EventGate: {}".format(e)
+            )
+            raise tornado.web.HTTPError(
+                status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+                reason=(
+                    "An unexpected error has occurred while trying "
+                    "to send the revision-score event to Eventgate. "
+                    "Please contact the ML team for more info."
+                ),
+            )
+
+    async def predict(self, request: Dict) -> Dict:
         feature_values = request.get(self.FEATURE_VAL_KEY)
         extended_output = request.get(self.EXTENDED_OUTPUT_KEY)
-        results = self.model.score(feature_values)
+        self.prediction_results = self.model.score(feature_values)
         if extended_output:
             # add extended output to reach feature parity with ORES, like:
             # https://ores.wikimedia.org/v3/scores/enwiki/186357639/goodfaith?features
             # If only rev_id is given in input.json, only the prediction results
             # will be present in the response. If the extended_output flag is true,
             # features output will be included in the response.
-            return {"predictions": results, "features": extended_output}
-        return {"predictions": results}
+            output = {
+                "predictions": self.prediction_results,
+                "features": extended_output,
+            }
+        else:
+            output = {"predictions": self.prediction_results}
+        # Send a revision-score event to EventGate, generated from
+        # the revision-create event passed as input.
+        if self.revision_create_event:
+            await self.send_event(self.revision_create_event)
+        return output
 
 
 if __name__ == "__main__":
