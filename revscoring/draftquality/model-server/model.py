5bbd8b446e984aadad711b4ccd83f394752c46d5
Luca Toscano <ltoscano@wikimedia.org>
2022-08-04 11:43:02 +0200

    draftquality: add code to send events to EventGate
    This is the same work done for editquality and articlequality,
    setting also Blubber's config to point to the new shared Python directory.
    
    Bug: T301878
    Change-Id: I4c76c6df9e9af406d43229e32d650cadb13ce9ed

--- revscoring/draftquality/model-server/model.py
+++ revscoring/draftquality/model-server/model.py
@@ -12,6 +12,9 @@ from revscoring.errors import RevisionNotFound
 from revscoring.extractors import api
 from revscoring.features import trim
 
+import events
+import preprocess_utils
+
 
 class DraftqualityModel(kserve.Model):
     def __init__(self, name: str):
@@ -20,6 +23,12 @@ class DraftqualityModel(kserve.Model):
         self.ready = False
         self.FEATURE_VAL_KEY = "feature_values"
         self.EXTENDED_OUTPUT_KEY = "extended_output"
+        self.REVISION_CREATE_EVENT_KEY = "revision_create_event"
+        self.EVENTGATE_URL = os.environ.get("EVENTGATE_URL")
+        self.EVENTGATE_STREAM = os.environ.get("EVENTGATE_STREAM")
+        self.CUSTOM_UA = "WMF ML team draftquality model"
+        # Deployed via the wmf-certificates package
+        self.TLS_CERT_BUNDLE_PATH = "/etc/ssl/certs/wmf-ca-certificates.crt"
 
     def load(self):
         with bz2.open("/mnt/models/model.bz2") as f:
@@ -28,7 +37,12 @@ class DraftqualityModel(kserve.Model):
 
     def preprocess(self, inputs: Dict) -> Dict:
         """Retrieve features from mediawiki."""
-        rev_id = self._get_rev_id(inputs)
+        rev_id = preprocess_utils.get_rev_id(inputs, self.REVISION_CREATE_EVENT_KEY)
+        # The predict() function needs to parse the revision_create_event
+        # given as input (if any).
+        self.revision_create_event = preprocess_utils.get_revision_event(
+            inputs, self.REVISION_CREATE_EVENT_KEY
+        )
         extended_output = inputs.get("extended_output", False)
         wiki_url = os.environ.get("WIKI_URL")
         wiki_host = os.environ.get("WIKI_HOST")
@@ -37,7 +51,7 @@ class DraftqualityModel(kserve.Model):
             s.headers.update({"Host": wiki_host})
         else:
             s = None
-        ua = "WMF ML team draftquality model"
+        ua = self.CUSTOM_UA
         self.extractor = api.Extractor(
             mwapi.Session(wiki_url, user_agent=ua, session=s)
         )
@@ -52,18 +66,42 @@ class DraftqualityModel(kserve.Model):
             }
         return inputs
 
-    def predict(self, request: Dict) -> Dict:
+    async def predict(self, request: Dict) -> Dict:
         feature_values = request.get(self.FEATURE_VAL_KEY)
         extended_output = request.get(self.EXTENDED_OUTPUT_KEY)
-        results = self.model.score(feature_values)
+        self.prediction_results = self.model.score(feature_values)
         if extended_output:
             # add extended output to reach feature parity with ORES, like:
             # https://ores.wikimedia.org/v3/scores/enwiki/1083325118/draftquality?features
             # If only rev_id is given in input.json, only the prediction results
             # will be present in the response. If the extended_output flag is true,
             # features output will be included in the response.
-            return {"predictions": results, "features": extended_output}
-        return {"predictions": results}
+            output = {
+                "predictions": self.prediction_results,
+                "features": extended_output,
+            }
+        else:
+            output = {
+                "predictions": self.prediction_results,
+            }
+        # Send a revision-score event to EventGate, generated from
+        # the revision-create event passed as input.
+        if self.revision_create_event:
+            revision_score_event = events.generate_revision_score_event(
+                self.revision_create_event,
+                self.EVENTGATE_STREAM,
+                self.model.version,
+                self.prediction_results,
+                "draftquality",
+            )
+            await events.send_event(
+                revision_score_event,
+                self.EVENTGATE_URL,
+                self.TLS_CERT_BUNDLE_PATH,
+                self.CUSTOM_UA,
+                self._http_client,
+            )
+        return output
 
     def _fetch_draftquality_features(self, rev_id: int) -> Dict:
         """Retrieve draftquality features."""
@@ -75,21 +113,6 @@ class DraftqualityModel(kserve.Model):
                 reason="Revision {} not found".format(rev_id),
             )
         return feature_values
-
-    def _get_rev_id(self, inputs: Dict) -> Dict:
-        try:
-            rev_id = inputs["rev_id"]
-        except KeyError:
-            raise tornado.web.HTTPError(
-                status_code=HTTPStatus.BAD_REQUEST,
-                reason='Expected "rev_id" in input data.',
-            )
-        if not isinstance(rev_id, int):
-            raise tornado.web.HTTPError(
-                status_code=HTTPStatus.BAD_REQUEST,
-                reason='Expected "rev_id" to be an integer.',
-            )
-        return rev_id
 
 
 if __name__ == "__main__":
