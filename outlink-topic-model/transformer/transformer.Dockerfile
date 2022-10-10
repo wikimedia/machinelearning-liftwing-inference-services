4194fcd8875b83a73b7ac404926dd08073f64233
AikoChou <achou@wikimedia.org>
2022-10-10 14:59:50 +0100

    Remove directories and scripts that are not used in production
    These directories and scripts were created to test inference services
    in ML-Sandbox and most of them have become stale.
    
    Bug: T320494
    Change-Id: I5a3bd4ce7ad984414edcb82d93ed21296e9444de

--- outlink-topic-model/transformer/transformer.Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM docker-registry.wikimedia.org/python3-build-buster:0.1.0
-
-COPY  . .
-RUN pip3 install --upgrade pip && pip install kfserving
-RUN pip3 install -e .
-ENTRYPOINT ["python3", "-m", "outlink_transformer"]
--- revscoring/editquality/enwiki-goodfaith/inference-client.sh
+++
@@ -1,5 +0,0 @@
-MODEL_NAME=enwiki-goodfaith
-INPUT_PATH=@./input.json
-CLIENT_GENERATORS_PATH=../../../client
-SESSION=$(python3 $CLIENT_GENERATORS_PATH/authservice-session-generator.py)
-. $CLIENT_GENERATORS_PATH/inference-generator.sh
