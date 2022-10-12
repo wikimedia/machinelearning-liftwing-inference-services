a4e46329678559e0a37590d73feb6fb9a09184f7
Luca Toscano <ltoscano@wikimedia.org>
2022-10-12 12:24:21 +0200

    topic: use the fetch_features shared function
    Bug: T320374
    Change-Id: Ieef0cd91b6cb103889aa894963761978f60334d4

--- revscoring/topic/model-server/model.py
+++ revscoring/topic/model-server/model.py
@@ -8,7 +8,6 @@ import mwapi
 import requests
 import tornado.web
 from revscoring import Model
-from revscoring.errors import RevisionNotFound
 from revscoring.extractors import api
 from revscoring.features import trim
 
@@ -62,10 +61,12 @@ class DrafttopicModel(kserve.Model):
                 mwapi.Session(wiki_url, user_agent=self.CUSTOM_UA),
                 http_cache=mw_http_cache,
             )
-            inputs[self.FEATURE_VAL_KEY] = self._fetch_topic_features(rev_id)
+            inputs[self.FEATURE_VAL_KEY] = extractor_utils.fetch_features(
+                rev_id, self.model.features, self.extractor
+            )
             if extended_output:
-                base_feature_values = self.extractor.extract(
-                    rev_id, list(trim(self.model.features))
+                base_feature_values = extractor_utils.fetch_features(
+                    rev_id, list(trim(self.model.features)), self.extractor
                 )
                 inputs[self.EXTENDED_OUTPUT_KEY] = {
                     str(f): v
@@ -111,18 +112,6 @@ class DrafttopicModel(kserve.Model):
                 self._http_client,
             )
         return output
-
-    def _fetch_topic_features(self, rev_id: int) -> Dict:
-        """Retrieve topic features."""
-        try:
-            feature_values = list(self.extractor.extract(rev_id, self.model.features))
-        except RevisionNotFound:
-            raise tornado.web.HTTPError(
-                status_code=HTTPStatus.BAD_REQUEST,
-                reason="Revision {} not found".format(rev_id),
-            )
-
-        return feature_values
 
 
 if __name__ == "__main__":
