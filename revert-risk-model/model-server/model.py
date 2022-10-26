3b5d0ec3c226584e863473023d6489fe58e8980b
AikoChou <achou@wikimedia.org>
2022-10-26 10:06:05 +0200

    revertrisk: add revertrisk model server and pipeline
    revert-risk-model is a language agnostic model to predict reverts
    on Wikipedia. The model server depends on the knowledge
    integrity package for feature extraction and classification
    developed by the research team.
    
    Bug: T321594
    Change-Id: I64c936133dc8a586bab2bbc3a60b94b3e2306b57

---
+++ revert-risk-model/model-server/model.py
@@ -0,0 +1,105 @@
+import asyncio
+import atexit
+import logging
+from typing import Any, Dict
+from http import HTTPStatus
+
+import aiohttp
+import kserve
+import mwapi
+import tornado.web
+
+from knowledge_integrity.models.revertrisk import load_model, classify
+from knowledge_integrity.revision import get_current_revision
+
+logging.basicConfig(level=kserve.constants.KSERVE_LOGLEVEL)
+
+
+class RevisionRevertRiskModel(kserve.Model):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self.name = name
+        self.ready = False
+        self._http_client_session = None
+        atexit.register(self._shutdown)
+
+    @property
+    def http_client_session(self):
+        if self._http_client_session is None or self._http_client_session.closed:
+            logging.info("Opening a new Asyncio session.")
+            self._http_client_session = aiohttp.ClientSession()
+        return self._http_client_session
+
+    def _shutdown(self):
+        if self._http_client_session and not self._http_client_session.closed:
+            logging.info("Closing asyncio session")
+            asyncio.run(self._http_client_session.close())
+
+    def load(self) -> None:
+        self.model = load_model("/mnt/models/model.pkl")
+        self.ready = True
+
+    async def preprocess(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
+        lang = inputs.get("lang")
+        rev_id = inputs.get("rev_id")
+        if lang is None:
+            raise tornado.web.HTTPError(
+                status_code=HTTPStatus.BAD_REQUEST,
+                reason="The parameter lang is required.",
+            )
+        if lang not in self.model.supported_wikis:
+            raise tornado.web.HTTPError(
+                status_code=HTTPStatus.BAD_REQUEST,
+                reason=f"Unsupported lang: {lang}.",
+            )
+        if rev_id is None:
+            raise tornado.web.HTTPError(
+                status_code=HTTPStatus.BAD_REQUEST,
+                reason="The parameter rev_id is required.",
+            )
+        session = mwapi.AsyncSession(
+            f"https://{lang}.wikipedia.org",
+            user_agent="WMF ML Team revert-risk-model isvc",
+            session=self.http_client_session,
+        )
+        try:
+            rev = await get_current_revision(session, rev_id, lang)
+            if rev is None:
+                raise tornado.web.HTTPError(
+                    status_code=HTTPStatus.BAD_REQUEST,
+                    reason=(
+                        f"Revision with lang: {lang} rev_id: {rev_id} "
+                        "missing information required for inference."
+                    ),
+                )
+        except Exception as e:
+            logging.error(
+                "An error has occurred while fetching info for revision: "
+                f" {rev_id} ({lang}). Reason: {e}"
+            )
+            raise tornado.web.HTTPError(
+                status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+                reason=(
+                    "An error happened while fetching info for revision "
+                    "from the MediaWiki API, please contact the ML-Team "
+                    "if the issue persists."
+                ),
+            )
+        inputs["revision"] = rev
+        return inputs
+
+    def predict(self, request: Dict[str, Any]) -> Dict[str, Any]:
+        result = classify(self.model, request["revision"])
+        return {
+            "prediction": result.prediction,
+            "probability": {
+                "true": result.probability,
+                "false": 1 - result.probability,
+            },
+        }
+
+
+if __name__ == "__main__":
+    model = RevisionRevertRiskModel("revert-risk-model")
+    model.load()
+    kserve.ModelServer(workers=1).start([model])
