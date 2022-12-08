7a6c19a1b72289d9045bf946d0075b709a0013da
AikoChou <achou@wikimedia.org>
2022-12-08 12:32:59 +0000

    Create a test folder and add lua scripts for wrk
    The lua script reads a file with multiple inputs and generate different
    requests when wrk executes. If specifing n threads, n log files will be
    created to log responses per thread. Also added a script for revscoring
    models.
    
    Guidelines are available in https://github.com/wg/wrk/blob/master/SCRIPTING
    
    Bug: T323613
    Change-Id: I36a6f7fd3c666fc40a54d098048c0b94928c17f6

---
+++ test/wrk/revertrisk.lua
@@ -0,0 +1,69 @@
+--[[
+     Usage:
+          wrk -c 4 -t 2 --timeout 5s -s revertrisk.lua \
+          https://inference.svc.eqiad.wmnet:30443/v1/models/revert-risk-model:predict \
+          --header "Host: revert-risk-model.experimental.wikimedia.org" --latency \
+          -- revertrisk.input
+--]]
+wrk.method = "POST"
+wrk.headers["Content-Type"] = "application/json"
+
+-- give each thread an id
+local counter = 1
+local threads = {}
+
+function setup(thread)
+     thread:set("id", counter)
+     table.insert(threads, thread)
+     counter = counter + 1
+end
+
+-- read an input file from command line arguments, parse it and store the data into an array of payloads
+-- create a log file for each thread
+function init(args)
+     file = io.open(args[1], "r");
+     data = {}
+     for line in file:lines() do
+          _, _, lang, rev_id = string.find(line, "(%a+)%s+(%d+)")
+          table.insert(data, '{"lang": "' .. lang .. '", "rev_id": ' .. rev_id ..'}');
+     end
+     file:close();
+     requests  = 0
+     responses = 0
+     local filename = "wrk_%d.log"
+     logfile = io.open(filename:format(id), "w");
+     local msg = "thread %d created logfile wrk_%d.log created"
+     print(msg:format(id, id))
+end
+
+-- (optional) add a random 10-50ms delay before each request
+function delay()
+     return math.random(10, 50)
+end
+
+-- change the body with different payload for each request
+i = 0
+function request()
+     requests = requests + 1
+     -- circle back to the first when used up the data
+     if i == #data then
+          i = 0
+     end
+     i = i + 1
+     return wrk.format(nil, nil, nil, data[i])
+end
+
+function response(status, headers, body)
+     responses = responses + 1
+     logfile:write("status:" .. status .. "\n" .. body .. "\n-------------------------------------------------\n");
+end
+
+function done(summary, latency, requests)
+     for index, thread in ipairs(threads) do
+        local id        = thread:get("id")
+        local requests  = thread:get("requests")
+        local responses = thread:get("responses")
+        local msg = "thread %d made %d requests and got %d responses"
+        print(msg:format(id, requests, responses))
+     end
+end
