4ae9054e24bd6901edd8a07a97e90db2afbfcd2a
isaranto <isarantopoulos@wikimedia.org>
2022-12-15 13:34:41 +0200

    revscoring: delete individual revscoring images
    Since we now have one docker image for all revscoring images that uses
    the same main file we are deprecating the separate blubber per model
    
    Bug: T323586
    Change-Id: Icc2d1b852ae57c1b5832922642fe29b1079e17ba

--- revscoring/topic/model-server/topic-prerequisites.sh
+++
@@ -1,7 +0,0 @@
-mkdir word2vec
-echo "check_certificate = off" >> ~/.wgetrc
-wget -O Makefile https://raw.githubusercontent.com/wikimedia/drafttopic/master/Makefile
-# download word embedding vectors from /srv/topic/Makefile by searching for strings that match the required targets.
-# i.e search(grep) strings that start with "word2vec/" and remove(sed) ":" from them.
-# also the first layer round brackets ( ) ensure that we don't leave the shell's current directory "/srv/topic"
-(make $(grep '^word2vec/.*' Makefile | sed 's/:.$//'))
