10da4ffb5c77275bf66d41dd65276d68fd218710
Luca Toscano <ltoscano@wikimedia.org>
2022-12-21 09:38:41 +0100

    Upgrade the revscoring model server to Python 3.9
    Using bullseye image and python 3.9.
     * Update spellchecking dictionaries from myspell to hunspell
     * upgrade packages in requirements
     * monkey patching pyenchant to be compatible with some older models
    
    Bug: T325657
    Change-Id: I5855f6c2eb41f8757781e3ebf97ba15884daf771

--- revscoring/model.py
+++ revscoring/model.py
@@ -1,9 +1,17 @@
 import os
-from distutils.util import strtobool
 
 import kserve
 from model_server_mp import RevscoringModelMP
 from model_servers import RevscoringModel, RevscoringModelType
+from distutils.util import strtobool
+import enchant
+from pyenchant_utils import UTF16EnchantStr, EnchantStr
+
+# monkey patching enchant to support older binaries. There are some older models
+# which have been trained with older enchant binaries. By including additional classes from v2.0.0
+# of the pyenchant library (the pyenchant_utils.py file), we allow these models to be loaded and used.
+enchant.utils.UTF16EnchantStr = UTF16EnchantStr
+enchant.utils.EnchantStr = EnchantStr
 
 if __name__ == "__main__":
     inference_name = os.environ.get("INFERENCE_NAME")
