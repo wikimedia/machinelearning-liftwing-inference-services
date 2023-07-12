85f1d4e6ca5af3709ec8f05d78f1b4df910d2b31
Ilias Sarantopoulos <isarantopoulos@wikimedia.org>
2023-07-12 19:38:14 +0300

    ores-legacy: configure logging level from env var
    Configure logging level from environment variable LOG_LEVEL. If the
    variable is not define then the default logging level is INFO.
    In order to facilitate this change the logging.ini file has been
    changed in a dict to enable dynamic env var loading.
    Also added some basic validation to ensure the app doesn't crash in
    case a wrong value is provided.
    
    Change-Id: I678d76050ba5d8e938c90cefcb5558930712275b

--- ores-legacy/app/__init__.py
+++ ores-legacy/app/__init__.py
@@ -1,9 +1,31 @@
 import logging.config
 import os
 
-logging.config.fileConfig(
-    os.path.join(
-        os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "logging.ini"
-    ),
-    disable_existing_loggers=False,
-)
+logging_level = os.getenv("LOG_LEVEL", "INFO").upper()
+if logging_level not in set(logging._nameToLevel.keys()):
+    logging.error(f"Logging level '{logging_level}' is not valid. Using INFO instead.")
+    logging_level = "INFO"
+
+
+logging_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "console": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"}
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": logging_level,
+            "formatter": "console",
+        }
+    },
+    "loggers": {
+        "": {
+            "level": logging_level,
+            "handlers": ["console"],
+        }
+    },
+}
+
+logging.config.dictConfig(logging_config)
