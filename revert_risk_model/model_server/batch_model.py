0695cd76596752eac8e6bad6852682c2d26b4430
AikoChou <achou@wikimedia.org>
2023-12-23 16:03:26 +0100

    revertrisk-batch: async fetching data and batch prediction
    * fetch data from MW API: `get_current_revisions` function is added
    to async request get_current_revision for multiple revisions. In this
    way, we don't need to change the function in knowledge_integrity.
    
    * predict: `classify_batch` is added to knowledge_integrity in
    https://gitlab.wikimedia.org/repos/research/knowledge_integrity/-/commit/026c11a7
    which allows for making predictions based on batch features.
    
    Bug: T352987
    Change-Id: Id4e38df5f1e64bf9012907d89cc77baa5f0b5993

--- revert_risk_model/model_server/batch_model.py
+++ revert_risk_model/model_server/batch_model.py
@@ -1,12 +1,13 @@
+import asyncio
 import logging
 from http import HTTPStatus
-from typing import Any, Dict
+from typing import Any, List, Dict, Sequence, Optional
 
 import mwapi
 from base_model import RevisionRevertRiskModel
 from fastapi import HTTPException
-from knowledge_integrity.revision import get_current_revision
-from kserve.errors import InferenceError
+from knowledge_integrity.revision import CurrentRevision, get_current_revision
+from kserve.errors import InferenceError, InvalidInput
 
 from python.preprocess_utils import validate_json_input
 
@@ -25,81 +26,96 @@ class RevisionRevertRiskModelBatch(RevisionRevertRiskModel):
             name, module_name, model_path, wiki_url, aiohttp_client_timeout, force_http
         )
 
+    async def get_current_revisions(
+        self, session: mwapi.AsyncSession, rev_ids: List[int], lang: str
+    ) -> Optional[Sequence[CurrentRevision]]:
+        tasks = [get_current_revision(session, rev_id, lang) for rev_id in rev_ids]
+        return await asyncio.gather(*tasks)
+
     async def preprocess(
         self, inputs: Dict[str, Any], headers: Dict[str, str] = None
     ) -> Dict[str, Any]:
         inputs = validate_json_input(inputs)
-        inputs["revision"] = []
-        for instance in inputs["instances"]:
-            lang = instance.get("lang")
-            rev_id = instance.get("rev_id")
-            logging.info(f"Received request for revision {rev_id} ({lang}).")
-            self.validate_inputs(lang, rev_id)
-            mw_host = self.get_mediawiki_host(lang)
-            session = mwapi.AsyncSession(
-                # Host is set to http://api-ro.discovery.wmnet within WMF
-                # network in Lift Wing. Alternatively, it can be set to
-                # https://{lang}.wikipedia.org to call MW API publicly.
-                host=self.wiki_url or mw_host,
-                user_agent="WMF ML Team revert-risk-model isvc",
-                session=self.get_http_client_session("mwapi"),
+        # During the experimental phase, we only accept requesting multiple
+        # rev_ids with the same lang. This allows us to use the same async
+        # session.
+        if len({x.get("lang") for x in inputs["instances"]}) > 1:
+            logging.error("More than one language in the request.")
+            raise InvalidInput(
+                "Requesting multiple revisions should have the same language."
             )
-            # Additional HTTP Host header must be set if the host is http://api-ro.discovery.wmnet
-            session.headers["Host"] = mw_host.replace("https://", "").replace(
-                "http://", ""
+        rev_ids = [x.get("rev_id") for x in inputs["instances"]]
+        lang = inputs["instances"][0].get("lang")
+        # TODO:
+        # self.validate_inputs(lang, rev_id)
+        mw_host = self.get_mediawiki_host(lang)
+        session = mwapi.AsyncSession(
+            # Host is set to http://api-ro.discovery.wmnet within WMF
+            # network in Lift Wing. Alternatively, it can be set to
+            # https://{lang}.wikipedia.org to call MW API publicly.
+            host=self.wiki_url or mw_host,
+            user_agent="WMF ML Team revert-risk-model isvc",
+            session=self.get_http_client_session("mwapi"),
+        )
+        # Additional HTTP Host header must be set if the host is http://api-ro.discovery.wmnet
+        session.headers["Host"] = mw_host.replace("https://", "").replace("http://", "")
+        try:
+            rev = await self.get_current_revisions(session, rev_ids, lang)
+        except Exception as e:
+            # TODO: think about how to do error handling for multiple rev_ids requests,
+            # For example, we want to (1) return predictions for the rest of valid
+            # revisions, even though there is one revision has problems (e.g. page
+            # missing), or (2) raise exception if any revision has problems
+            logging.error(
+                "An error has occurred while fetching info for revision: "
+                f" {rev_ids} ({lang}). Reason: {e}"
             )
-            try:
-                rev = await get_current_revision(session, rev_id, lang)
-            except Exception as e:
-                logging.error(
-                    "An error has occurred while fetching info for revision: "
-                    f" {rev_id} ({lang}). Reason: {e}"
-                )
-                raise InferenceError(
-                    "An error happened while fetching info for revision "
-                    "from the MediaWiki API, please contact the ML-Team "
-                    "if the issue persists."
-                )
-            if rev is None:
-                logging.error(
-                    "get_current_revision returned empty results "
-                    f"for revision {rev_id} ({lang})"
-                )
-                raise HTTPException(
-                    status_code=HTTPStatus.BAD_REQUEST,
-                    detail=(
-                        "The necessary features cannot be obtained from the "
-                        "MediaWiki API. It can be the revision, parent revision, "
-                        "page information, or user information. This could be "
-                        "because the data does not exist or has been deleted."
-                    ),
-                )
-            inputs["revision"].append(rev)
+            raise InferenceError(
+                "An error happened while fetching info for revision "
+                "from the MediaWiki API, please contact the ML-Team "
+                "if the issue persists."
+            )
+        # TODO: same as above
+        if rev is None:
+            logging.error(
+                "get_current_revision returned empty results "
+                f"for revision {rev_ids} ({lang})"
+            )
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail=(
+                    "The necessary features cannot be obtained from the "
+                    "MediaWiki API. It can be the revision, parent revision, "
+                    "page information, or user information. This could be "
+                    "because the data does not exist or has been deleted."
+                ),
+            )
+        inputs["revision"] = rev
         return inputs
 
     def predict(
         self, request: Dict[str, Any], headers: Dict[str, str] = None
     ) -> Dict[str, Any]:
-        predictions = []
-        num_inst = len(request["instances"])
-        logging.info(f"Getting {num_inst} rev_ids in the request")
-        for i in range(num_inst):
-            result = self.ModelLoader.classify(self.model, request["revision"][i])
-            if self.name == "revertrisk-wikidata":
-                edit_summary = request["revision"][i].comment
-                self.check_wikidata_result(result, edit_summary)
-            prediction = {
+        n_rev = len(request["revision"])
+        logging.info(f"Getting {n_rev} rev_ids in the request")
+        results = self.ModelLoader.classify_batch(self.model, request["revision"])
+        if self.name == "revertrisk-wikidata":
+            for i, results in enumerate(results):
+                self.check_wikidata_result(results[i], request["revision"][i].comment)
+        predictions = [
+            {
                 "model_name": self.name,
                 "model_version": str(self.model.model_version),
-                "wiki_db": request["instances"][i].get("lang") + "wiki",
-                "revision_id": request["instances"][i].get("rev_id"),
+                "wiki_db": request["revision"][i].lang + "wiki",
+                "revision_id": request["revision"][i].id,
                 "output": {
-                    "prediction": result.prediction,
+                    "prediction": results[i].prediction,
                     "probabilities": {
-                        "true": result.probability,
-                        "false": 1 - result.probability,
+                        "true": results[i].probability,
+                        "false": 1 - results[i].probability,
                     },
                 },
             }
-            predictions.append(prediction)
+            for i in range(len(results))
+        ]
         return {"predictions": predictions}
