2d45e70e4e0a6d442a36866432a23432c53d0e1e
Ilias Sarantopoulos <isarantopoulos@wikimedia.org>
2024-04-25 19:26:10 +0300

    utils: slow function execution wrapper
    Add a decorator that can be used both for async and sync functions that will log
    a warning message if it passes the threshold (a threshold can be passed as an
    argument to the decorator).
    
    The purpose is to use this decorator so that we can debug functions that take too
    much time to execute.
    
    Bug: T362663
    Change-Id: Iff435042c2c73fca5cf8905fa9ce9e94ab565403

--- python/decorators.py
+++ python/decorators.py
@@ -1,3 +1,5 @@
+import asyncio
+import json
 import logging
 import time
 from functools import wraps
@@ -14,7 +16,7 @@ def elapsed_time_async(func):
         result = await func(*args, **kwargs)
         end = time.perf_counter()
         total = end - start
-        logging.info(f"Function {func.__name__} took {total:.4f} seconds to execute.")
+        logging.info(f"Function {func.__name__} took {total:.2f} seconds to execute.")
         return result
 
     return elapsed_time_wrapper
@@ -35,3 +37,51 @@ def elapsed_time(func):
         return result
 
     return elapsed_time_wrapper
+
+
+def log_slow_function(threshold: float = 10.0):
+    """Decorator to log function that takes longer than threshold seconds. This decorator works both
+    for synchronous and asynchronous functions."""
+
+    def decorator(func):
+        @wraps(func)
+        def log_slow_requests_wrapper(*args, **kwargs):
+            start = time.perf_counter()
+            result = func(*args, **kwargs)
+            end = time.perf_counter()
+            total = end - start
+
+            args_json = json.dumps(args)
+            kwargs_json = json.dumps(kwargs)
+
+            if total > threshold:
+                logging.warning(
+                    f"Function {func.__name__} called with args: {args_json}, kwargs: {kwargs_json}. "
+                    f"Took {total:.2f} seconds to execute."
+                )
+
+            return result
+
+        async def log_slow_requests_wrapper_async(*args, **kwargs):
+            start = time.perf_counter()
+            result = await func(*args, **kwargs)
+            end = time.perf_counter()
+            total = end - start
+
+            args_json = json.dumps(args)
+            kwargs_json = json.dumps(kwargs)
+
+            if total > threshold:
+                logging.warning(
+                    f"Function {func.__name__} called with args: {args_json}, kwargs: {kwargs_json}. "
+                    f"Took {total:.2f} seconds to execute."
+                )
+
+            return result
+
+        if asyncio.iscoroutinefunction(func):
+            return log_slow_requests_wrapper_async
+        else:
+            return log_slow_requests_wrapper
+
+    return decorator
---
+++ test/unit/python/test_decorators.py
@@ -0,0 +1,26 @@
+import pytest
+import time
+from python.decorators import log_slow_function
+
+
+@log_slow_function(threshold=0.1)
+def dummy_function(arg1: int, arg2: int, sleep_seconds: float = 0.2):
+    """This is a dummy function to test the decorator"""
+    time.sleep(sleep_seconds)
+
+
+def test_log_slow_function(caplog):
+    """
+    Test that the log_slow_function decorator logs a message when the decorated function
+    takes longer than the threshold
+    """
+    seconds_to_wait = 0.2
+    _ = dummy_function(1, 2, sleep_seconds=seconds_to_wait)
+
+    # Check that the log message is captured
+    expected_log_message = f"""Function dummy_function called with args: [1, 2], kwargs: {{"sleep_seconds": {seconds_to_wait}}}."""
+    assert expected_log_message in caplog.text
+
+
+if __name__ == "__main__":
+    pytest.main()
