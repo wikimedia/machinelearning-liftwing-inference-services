65dac0639f7a9e1229230fd65ce619b6cfd1d24b
AikoChou <achou@wikimedia.org>
2024-06-17 17:33:08 +0200

    articlequality: add feature preprocess
    - Add a function called load_quality_max_featurevalues() to read
    the table of maximum feature values used for feature normalization.
    There is one table in the data/ directory, which is used in the
    notebook. It contains data from 6 wikis and includes new features
    (max_srcs, infobox, and mbox).
    
    - Functions such as get_article_features and normalize_features, which
    come from the notebook, remain unchanged.
    
    - get_article_html from the notebook is used to obtain an article
    revision’s HTML. This should be modified to an asynchronous function
    using the aiohttp session.
    
    Bug: T360455
    Change-Id: I36e854b6318748bf7018a4e2eda17c7e296093c7

--- src/models/articlequality/model_server/model.py
+++ src/models/articlequality/model_server/model.py
@@ -3,8 +3,15 @@ import os
 from typing import Any, Dict
 
 import kserve
+from kserve.errors import InferenceError
 
 from python.preprocess_utils import validate_json_input
+from utils import (
+    get_article_html,
+    get_article_features,
+    normalize_features,
+    load_quality_max_featurevalues,
+)
 
 
 logging.basicConfig(level=kserve.constants.KSERVE_LOGLEVEL)
@@ -20,13 +27,32 @@ class ArticleQualityModel(kserve.Model):
 
     def load(self) -> None:
         # TODO: Load the model from the model_path
+
+        # Load the table of max feature values - use for feature normalization
+        self.max_qual_vals = load_quality_max_featurevalues(
+            "data/max-vals-html-dumps-ar-en-fr-hu-tr-zh.tsv"
+        )
         self.ready = True
 
     async def preprocess(
         self, inputs: Dict[str, Any], headers: Dict[str, str] = None
     ) -> Dict[str, Any]:
-        # TODO: Preprocess features
         inputs = validate_json_input(inputs)
+        lang = inputs.get("lang")
+        rev_id = inputs.get("rev_id")
+        article_html = get_article_html(lang, rev_id)
+        article_features = get_article_features(article_html)
+        if article_features[0] > 0:  # page_length > 0
+            normalized_features = normalize_features(
+                self.max_qual_vals, lang, *article_features
+            )
+        else:
+            raise InferenceError(
+                f"Article with language {lang} and revid {rev_id}"
+                " has errors when preprocessing"
+            )
+
+        inputs["normalized_features"] = normalized_features
         return inputs
 
     def predict(
---
+++ src/models/articlequality/model_server/utils.py
@@ -0,0 +1,204 @@
+import math
+import pandas as pd
+
+import requests
+from mwparserfromhtml import Article
+from mwparserfromhtml.parse.utils import is_transcluded
+
+
+def get_article_html(lang, revid):
+    """Get an article revision's HTML.
+
+    NOTE: fetching HTML for old revisions can be slow as it's not always cached.
+    Here we use exact revision IDs.
+
+    See: https://www.mediawiki.org/wiki/RESTBase/service_migration#Parsoid_endpoints
+
+    """
+    base_url = f"https://{lang}.wikipedia.org/w/rest.php/v1/revision/{revid}/html"  # returns the html contents
+
+    try:
+        response = requests.get(
+            base_url, headers={"User-Agent": "liftwing articlequality prototype"}
+        )
+        return response.text
+    except Exception:
+        return None
+
+
+def _html_to_plaintext(article):
+    """Convert Parsoid HTML to reasonable plaintext."""
+    exclude_transcluded_paragraphs = True
+    exclude_elements = {
+        "Category",
+        "Citation",
+        "Comment",
+        "Heading",
+        "Infobox",
+        "Math",
+        "Media-audio",
+        "Media-img",
+        "Media-video",
+        "Messagebox",
+        "Navigational",
+        "Note",
+        "Reference",
+        "TF-sup",  # superscript -- catches Citation-needed tags etc.
+        "Table",
+        "Wikitable",
+    }
+    exclude_para_context = {"pre-first-para", "between-paras", "post-last-para"}
+
+    paragraphs = [
+        paragraph.strip()
+        for heading, paragraph in article.wikistew.get_plaintext(
+            exclude_transcluded_paragraphs=exclude_transcluded_paragraphs,
+            exclude_para_context=exclude_para_context,
+            exclude_elements=exclude_elements,
+        )
+    ]
+
+    return "\n".join(paragraphs) if paragraphs else None
+
+
+def get_article_features(article_html):
+    try:
+        article = Article(article_html)
+    except TypeError:
+        print(f"Skipping article due to TypeError: {article_html}")
+        return 0, 0, 0, 0, 0, 0, 0, 0, 0
+
+    plaintext = _html_to_plaintext(article)
+    page_length = len(plaintext) if plaintext else 0
+    refs = len(article.wikistew.get_citations())
+    wikilinks_objects = [
+        w for w in article.wikistew.get_wikilinks() if not is_transcluded(w.html_tag)
+    ]
+    wikilinks = len(
+        [
+            wikilink.link.lstrip(".")
+            for wikilink in wikilinks_objects
+            if not wikilink.link.endswith("redlink=1")
+        ]
+    )
+    categories = len(
+        [1 for c in article.wikistew.get_categories() if not is_transcluded(c.html_tag)]
+    )
+    max_icon_pixel_area = 100 * 100  # 10000 pixels
+    article_images = [
+        image
+        for image in article.wikistew.get_images()
+        if image.height * image.width > max_icon_pixel_area
+    ]
+    article_videos = [video for video in article.wikistew.get_video()]
+    article_audio = [audio for audio in article.wikistew.get_audio()]
+    media = len(article_images) + len(article_videos) + len(article_audio)
+    headings = len([h for h in article.wikistew.get_headings() if h.level <= 3])
+
+    # new additions
+    # unique sources
+    sources = len(article.wikistew.get_references())
+
+    # infoboxes, if len >= 1, then return true else false
+    infoboxes = True if len(article.wikistew.get_infobox()) >= 1 else False
+
+    # message boxes, return true if len >= 1 else false
+    messageboxes = True if len(article.wikistew.get_message_boxes()) >= 1 else False
+
+    return [
+        page_length,
+        refs,
+        wikilinks,
+        categories,
+        media,
+        headings,
+        sources,
+        infoboxes,
+        messageboxes,
+    ]
+
+
+def normalize_features(
+    MAX_QUAL_VALS,
+    lang,
+    page_length,
+    num_refs,
+    num_wikilinks,
+    num_cats,
+    num_media,
+    num_headings,
+    num_sources,
+    num_infoboxes,
+    num_messageboxes,
+):
+    """Convert raw count features into values between 0 and 1.
+
+    Possible transformations:
+    * square root: make initial gains in feature of more importance to model
+                   e.g., going from 0 -> 16 wikilinks is the same as 16 -> 64
+    * divide by page length: convert absolutes to an expectation per byte of content
+                             e.g., total references -> references per paragraph
+    """
+    normed_page_length = math.sqrt(page_length)
+    length_x = min(1, normed_page_length / MAX_QUAL_VALS[lang]["max_length"])
+    refs_x = min(1, (num_refs / normed_page_length) / MAX_QUAL_VALS[lang]["max_refs"])
+    wikilinks_x = min(
+        1, (num_wikilinks / normed_page_length) / MAX_QUAL_VALS[lang]["max_links"]
+    )  # The squareRoot dropped
+    categories_x = min(1, num_cats / MAX_QUAL_VALS[lang]["max_cats"])
+    media_x = min(1, num_media / MAX_QUAL_VALS[lang]["max_media"])
+    headings_x = min(
+        1, (num_headings / normed_page_length) / MAX_QUAL_VALS[lang]["max_headings"]
+    )
+
+    # new additions
+    sources_x = min(1, num_sources / MAX_QUAL_VALS[lang]["max_srcs"])
+    infoboxes_x = num_infoboxes
+    messageboxes_x = num_messageboxes
+
+    return (
+        length_x,
+        refs_x,
+        wikilinks_x,
+        categories_x,
+        media_x,
+        headings_x,
+        sources_x,
+        infoboxes_x,
+        messageboxes_x,
+    )
+
+
+def load_quality_max_featurevalues(file_path):
+    feature_names = [
+        "max_length",
+        "max_media",
+        "max_cats",
+        "max_refs",
+        "max_headings",
+        "max_links",
+        "max_srcs",
+    ]
+
+    MIN_MAX_LEN = 45  # changed from 100 to 45
+    MIN_MAX_MED = 2
+    MIN_MAX_CAT = 5
+    MIN_MAX_REF = 0.2  # changed from 0.15 to 0.2
+    MIN_MAX_HEA = 0.1
+    MIN_MAX_LIN = 0.45  # changed from 0.1 to 0.45
+    MIN_MAX_UNIQUE_SOURCES = 5  # added sources
+
+    df = pd.read_csv(file_path, sep="\t")
+    df["lang"] = df["wiki_db"].apply(lambda x: x.replace("wiki", ""))
+    df["max_length"] = df["max_length"].apply(lambda x: max(MIN_MAX_LEN, float(x)))
+    df["max_media"] = df["max_media"].apply(lambda x: max(MIN_MAX_MED, float(x)))
+    df["max_cats"] = df["max_cats"].apply(lambda x: max(MIN_MAX_CAT, float(x)))
+    df["max_refs"] = df["max_refs"].apply(lambda x: max(MIN_MAX_REF, float(x)))
+    df["max_headings"] = df["max_headings"].apply(lambda x: max(MIN_MAX_HEA, float(x)))
+    df["max_links"] = df["max_links"].apply(lambda x: max(MIN_MAX_LIN, float(x)))
+    df["max_srcs"] = df["max_srcs"].apply(
+        lambda x: max(MIN_MAX_UNIQUE_SOURCES, float(x))
+    )
+
+    df = df.set_index("lang")
+    return df[feature_names].T.to_dict()
