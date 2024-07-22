4ea4dfc5df02960ee2ef129e5b7940e594487f3c
Ilias Sarantopoulos <isarantopoulos@wikimedia.org>
2024-07-22 19:10:01 +0300

    huggingface: accept cmd args in docker entrypoint
    Add the ability to specify additional cmd arguments in the docker image entrypoint like dtype and backend without having to override the whole command.
    
    Bug: T370670
    Change-Id: I05db21551e06057b8f0abcdc6801ac93440ca368

--- huggingface_modelserver/entrypoint.sh
+++ huggingface_modelserver/entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 if [ -z "$MODEL_ID" ]; then
     # Run the server from a local model directory
-    python3 -m huggingfaceserver --model_dir $MODEL_DIR --model_name $MODEL_NAME
+    python3 -m huggingfaceserver --model_dir $MODEL_DIR --model_name $MODEL_NAME "${@}"
 else
     # Run the server by downloading the model from HuggingFace
-    python3 -m huggingfaceserver --model_id $MODEL_ID --model_name $MODEL_NAME
+    python3 -m huggingfaceserver --model_id $MODEL_ID --model_name $MODEL_NAME "${@}"
 fi
