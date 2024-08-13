cfa3110c27f1d913a679f4a5ad83bd1a0eb6b079
AikoChou <qwanqwanro@gmail.com>
2024-08-13 13:38:47 +0000

    locust: entry for readability model
    Bug: T369712
    Change-Id: I5c76d6bbdfd7366e6a6fa2ae0754fff860943baf

--- test/locust/models/__init__.py
+++ test/locust/models/__init__.py
@@ -1,5 +1,6 @@
 from .article_descriptions import *  # noqa
 from .huggingface import *  # noqa
 from .logo_detection import *  # noqa
+from .readability import *  # noqa
 from .revertrisk import *  # noqa
 from .revscoring import *  # noqa
---
+++ test/locust/models/readability/readability.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+from locust import FastHttpUser, between, task
+
+rev_ids = pd.read_csv("inputs/revertrisk.input", delimiter="\t", header=None)
+
+
+def get_random_sample_from_df_input(df):
+    lang, rev_id = df.sample(n=1).squeeze().tolist()
+    return lang, rev_id
+
+
+class Readability(FastHttpUser):
+    wait_time = between(1, 5)
+
+    @task(3)
+    def get_prediction(self):
+        lang, rev_id = get_random_sample_from_df_input(rev_ids)
+        headers = {
+            "Content-Type": "application/json",
+            "Host": "readability.readability.wikimedia.org",
+        }
+        self.client.post(
+            "/v1/models/readability:predict",
+            json={"rev_id": int(rev_id), "lang": lang},
+            headers=headers,
+        )
