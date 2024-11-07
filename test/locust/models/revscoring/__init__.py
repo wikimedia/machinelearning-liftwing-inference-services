ef721e119d8fe436d7067df73c97fe9e49178f34
Ilias Sarantopoulos <isarantopoulos@wikimedia.org>
2024-11-07 12:24:01 +0200

    locust: add test for enwiki damaging
    Change-Id: I7bb7f819363aa9b7c422df8001fc079e6f81d738

--- test/locust/models/revscoring/__init__.py
+++ test/locust/models/revscoring/__init__.py
@@ -1,1 +1,2 @@
 from .goodfaith.goodfaith import EnwikiGoodfaith  # noqa
+from .damaging.damaging import EnwikiDamaging  # noqa
---
+++ test/locust/models/revscoring/damaging/damaging.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+from locust import FastHttpUser, between, task
+
+rev_ids = pd.read_csv("data/enwiki_revids.tsv", delimiter="\t", header=None)
+
+
+def get_random_sample_from_df_input(df):
+    rev_id = df.sample(n=1).squeeze().tolist()
+    return rev_id
+
+
+class EnwikiDamaging(FastHttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def get_prediction(self):
+        headers = {
+            "Content-Type": "application/json",
+            "Host": "enwiki-damaging.revscoring-editquality-damaging.wikimedia.org",
+        }
+        rev_id = get_random_sample_from_df_input(rev_ids)
+        self.client.post(
+            "/v1/models/enwiki-damaging:predict",
+            json={"rev_id": int(rev_id)},
+            headers=headers,
+        )
