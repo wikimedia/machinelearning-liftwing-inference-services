5fd7d57454decaa703b5ea423c6a9425e0ccac81
kevinbazira <kvnbzr@gmail.com>
2024-11-28 07:06:56 +0300

    ci: remove backward compatibility from entrypoint
    This patch removes support for older test variants, as all test variants
    that previously had tox.ini in a specific dir have now been updated, as
    detailed in T360120. The entrypoint now assumes that tox.ini is in the
    root dir to cater to only the new test variants.
    
    Bug: T360120
    Change-Id: I22d47e8b6f9d0d01b5d9f00736b82207ece70651

--- ci_entrypoint.sh
+++ ci_entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 git init
 git add .
-if [[ "$1" == "model-server" || "$1" == "model_server" || "$1" == "transformer" || "$1" == "revscoring_model" || "$1" == "." ]]; then
-  # for backward compatibility, temporarily accommodate older test variants that still have tox.ini in a specific dir
-  tox -c "$1/tox.ini" -e ci-lint
-else
-  # cater to new test variants that have tox.ini in root dir
-  tox -e "$1,$2"
-fi
+
+# cater to new test variants that have tox.ini in root dir
+tox -e "$1,$2"
