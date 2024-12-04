b477d8f52ea6067edd5c86289aecfec16c6cf3db
Ilias Sarantopoulos <isarantopoulos@wikimedia.org>
2024-12-04 14:05:05 +0200

    llm: add aya with bitsandbytes
    * add a class for the aya model family in the llm model server
    * install the bitsandbytes alpha release that supports ROCm https://huggingface.co/docs/bitsandbytes/main/en/installation?platform=ROCm#multi-backend
    * set an env var for device_map
    * set an env var for quantization dtype to work around the fact that 4bit/8bit
    refer to different constructor arguments in BitsAndBytesConfig
    
    Bug: T379052
    Change-Id: Ie3934de31a6a51f3342cd111b00c477c15c8618f

---
+++ src/models/llm/aya/aya.py
@@ -0,0 +1,34 @@
+import os
+from typing import Tuple
+
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+
+from src.models.llm import LLM
+
+
+class Aya(LLM):
+    def __init__(self, model_name: str):
+        self.quantization_mode = os.environ.get("BITSANDBYTES_DTYPE", "int4")
+        self.device = os.environ.get("DEVICE", "cuda:0")
+        super().__init__(model_name)
+
+    def load(self) -> Tuple[AutoModelForCausalLM, AutoTokenizer]:
+        if self.quantization_mode == "int4":
+            quantization_config = BitsAndBytesConfig(load_in_4bit=True)
+        elif self.quantization_mode == "int8":
+            quantization_config = BitsAndBytesConfig(load_in_8bit=True)
+        else:
+            quantization_config = None
+        model = AutoModelForCausalLM.from_pretrained(
+            self.model_path,
+            device=self.device,
+            local_files_only=True,
+            low_cpu_mem_usage=True,
+            trust_remote_code=True,
+            quantization_config=quantization_config,
+        )
+        tokenizer = AutoTokenizer.from_pretrained(
+            self.model_path, local_files_only=True
+        )
+        self.ready = True
+        return model, tokenizer
