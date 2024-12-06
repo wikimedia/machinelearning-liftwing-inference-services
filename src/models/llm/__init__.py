fdcb6a2d47a736039fb146aefda93f8170fcc23d
Ilias Sarantopoulos <isarantopoulos@wikimedia.org>
2024-12-06 10:52:31 +0200

    llm: add aya to __init__
    Bug: T379052
    Change-Id: I4d086fa628f75d20f70d210fd683a77c68f31591

--- src/models/llm/__init__.py
+++ src/models/llm/__init__.py
@@ -1,6 +1,7 @@
+from aya.aya import Aya
 from nllb.nllb import NLLB
 from nllb.nllb_cpu import NLLBCTranslate
 
 from src.models.llm.model import LLM
 
-__all__ = ["LLM", "NLLB", "NLLBCTranslate"]
+__all__ = ["Aya", "LLM", "NLLB", "NLLBCTranslate"]
