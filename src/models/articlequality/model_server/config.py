8470ec19379378cbc623c46accd0e01681c65c26
ozge <okarakaya@wikimedia.org>
2025-04-28 14:19:35 +0200

    feat: updates blubber yaml for  articlequality_v2
    -  configures to read from src.
    
    Change-Id: I724e9a484e2118b28adab3c0e3ad32d864b3452a

--- src/models/articlequality/model_server/config.py
+++ src/models/articlequality/model_server/config.py
@@ -11,7 +11,7 @@ If environment variables do not exist, it uses the default values.
 class Settings(BaseSettings):
     model_name: str = "articlequality"
     model_path: str = "/mnt/models/model.pkl"
-    max_feature_vals: str = "data/feature_values.tsv"
+    max_feature_vals: str = "src/models/articlequality/data/feature_values.tsv"
     force_http: bool = False
     model_name_v2: str = "articlequality_v2"
     model_path_v2: str = "/mnt/models/catboost_model.cbm"
