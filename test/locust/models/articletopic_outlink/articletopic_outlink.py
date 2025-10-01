8bdf0b093ed1a0445a862b150762aebc097aa7ec
Bartosz Wójtowicz <bwojtowicz@wikimedia.org>
2025-10-01 10:15:50 +0200

    articletopic: Update locust tests to use both `page_id` and `page_title`.
    Bug: T371021
    Change-Id: I06999dce525ec072a28a9ec2fec3e3afdb1d5eef

--- test/locust/models/articletopic_outlink/articletopic_outlink.py
+++ test/locust/models/articletopic_outlink/articletopic_outlink.py
@@ -2,20 +2,20 @@ import pandas as pd
 
 from locust import FastHttpUser, between, task
 
-articles = pd.read_csv("data/articles_lang_and_title.csv")
+articles = pd.read_csv("data/articles_lang_title_id.csv")
 
 
 def get_random_sample_from_df_input(df):
-    lang, title = df.sample(n=1).squeeze().tolist()
-    return lang, title
+    lang, page_title, page_id = df.sample(n=1).squeeze().tolist()
+    return lang, page_title, page_id
 
 
 class ArticleTopicOutlink(FastHttpUser):
     wait_time = between(1, 5)
 
     @task
-    def get_prediction(self):
-        lang, title = get_random_sample_from_df_input(articles)
+    def get_prediction_title(self):
+        lang, page_title, page_id = get_random_sample_from_df_input(articles)
         headers = {
             "Content-Type": "application/json",
             "Host": "outlink-topic-model.articletopic-outlink.wikimedia.org",
@@ -24,7 +24,23 @@ class ArticleTopicOutlink(FastHttpUser):
             "/v1/models/outlink-topic-model:predict",
             json={
                 "lang": lang,
-                "page_title": title,
+                "page_title": page_title,
+            },
+            headers=headers,
+        )
+
+    @task
+    def get_prediction_id(self):
+        lang, page_title, page_id = get_random_sample_from_df_input(articles)
+        headers = {
+            "Content-Type": "application/json",
+            "Host": "outlink-topic-model.articletopic-outlink.wikimedia.org",
+        }
+        self.client.post(
+            "/v1/models/outlink-topic-model:predict",
+            json={
+                "lang": lang,
+                "page_id": int(page_id),
             },
             headers=headers,
         )
