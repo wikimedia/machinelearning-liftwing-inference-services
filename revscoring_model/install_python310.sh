d4dfef05e982f63fa23918b5a54428466d63f67f
AikoChou <achou@wikimedia.org>
2026-04-23 20:59:42 +0200

    revscoring: fix SudachiPy 0.5.2 build
    Pre-install Cython==0.29.24 and use --no-build-isolation +
    SETUPTOOLS_SCM_PRETEND_VERSION=0.5.2 to work around Cython 3.x
    crash and broken version detection when building from source on cp310.
    
    Also add enwiki-goodfaith service to docker-compose.yml for local testing.
    
    Ref: https://integration.wikimedia.org/ci/job/inference-services-pipeline-revscoring-publish/120/console
    
    Bug: T416384
    Change-Id: I5e14e65f5804aa9e0e4d942d1e52df460a17091f

--- revscoring_model/install_python310.sh
+++ revscoring_model/install_python310.sh
@@ -14,5 +14,14 @@ make altinstall
 cd /srv/revscoring/
 /srv/revscoring/python3.10/bin/python3.10 -m ensurepip
 /srv/revscoring/python3.10/bin/python3.10 -m pip install --upgrade pip
+# Pre-install build tools so SudachiPy can be compiled from source below.
+/srv/revscoring/python3.10/bin/python3.10 -m pip install "Cython==0.29.24" wheel setuptools_scm
+# SudachiPy 0.5.2 has two issues when pip installs it via its normal isolated build env:
+#   1. Cython 3.x (resolved at build time) crashes on tokenizer.pyx; --no-build-isolation
+#      forces use of the Cython==0.29.24 already installed above.
+#   2. setuptools_scm falls back to version "0.0.0" without git tags; the env var overrides it.
+SETUPTOOLS_SCM_PRETEND_VERSION=0.5.2 \
+    /srv/revscoring/python3.10/bin/python3.10 -m pip install --no-build-isolation "SudachiPy==0.5.2"
+# Install remaining requirements; SudachiPy is already satisfied and will be skipped.
 /srv/revscoring/python3.10/bin/python3.10 -m pip install -r /srv/revscoring/revscoring_model/requirements.txt
 /srv/revscoring/python3.10/bin/python3.10 -m nltk.downloader omw sentiwordnet stopwords wordnet omw-1.4
