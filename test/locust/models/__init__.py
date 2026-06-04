0d757a6614deffc54199c974e47c5113c6f62baa
ozge <okarakaya@wikimedia.org>
2026-06-04 16:33:49 +0200

    Adds editing-suggestions locust test
    Bug: T427794
    Change-Id: I659a384b517ac8bbdf72bbffe503e58dfe644de4

--- test/locust/models/__init__.py
+++ test/locust/models/__init__.py
@@ -2,6 +2,7 @@ from .article_country import *  # noqa
 from .article_descriptions import *  # noqa
 from .articlequality import *  # noqa
 from .articletopic_outlink import *  # noqa
+from .editing_suggestions import *  # noqa
 from .embeddings import *  # noqa
 from .huggingface import *  # noqa
 from .logo_detection import *  # noqa
---
+++ test/locust/models/editing_suggestions/editing_suggestions_test.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+from locust import FastHttpUser, between, task
+
+pages = pd.read_csv("data/editing_suggestions_locust.csv")
+
+
+def get_random_sample_from_df_input(df):
+    page_title, wiki_id = df.sample(n=1).squeeze().tolist()
+    return page_title, wiki_id
+
+
+class EditingSuggestions(FastHttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def get_prediction(self):
+        page_title, wiki_id = get_random_sample_from_df_input(pages)
+        headers = {
+            "Content-Type": "application/json",
+            "Host": "editing-suggestions.experimental.wikimedia.org",
+        }
+        self.client.post(
+            "/v1/models/editing-suggestions:predict",
+            json={"wiki_id": wiki_id, "page_title": page_title},
+            headers=headers,
+        )
